8eddb8fa2f6d95d27993908459cff5f4f200c60c
redthing1 <redthing1@alt.icu>
2020-07-21 23:30:50 -0700

    split position and position2

--- src/source/re/math/transform.d
+++ src/source/re/math/transform.d
@@ -5,13 +5,38 @@ public import raylib : Vector2, Vector3;
 struct Transform {
     private bool _dirty;
 
-    public Vector2 position;
+    private Vector3 _position;
+    private float _rotation;
 
-    // @property Vector2 position() {
-    //     return transform.position;
-    // }
+    @property Vector2 position2() {
+        auto pos = position;
+        return Vector2(pos.x, pos.y);
+    }
 
-    // @property Vector2 position(Vector2 value) {
-    //     transform.position = value;
-    // }
+    @property Vector2 position2(Vector2 value) {
+        position = Vector3(value.x, value.y, 0);
+        return value;
+    }
+
+    @property Vector3 position() {
+        update_transform();
+        return _position;
+    }
+
+    @property Vector3 position(Vector3 value) {
+        return _position = value;
+    }
+
+    @property float rotation() {
+        update_transform();
+        return _rotation;
+    }
+
+    @property float rotation(float radians) {
+        return _rotation = radians;
+    }
+
+    private void update_transform() {
+
+    }
 }
