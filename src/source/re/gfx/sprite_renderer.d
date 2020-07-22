03d9885de00cb2c81c9672dc86b27d48fb4f0d2b
redthing1 <redthing1@alt.icu>
2020-07-22 15:39:53 -0700

    directly use src_rect field

--- src/source/re/gfx/sprite_renderer.d
+++ src/source/re/gfx/sprite_renderer.d
@@ -12,20 +12,14 @@ class SpriteRenderer : Component, Renderable {
         this.sprite = sprite;
     }
 
-    private Rectangle sprite_tex_rect() {
-        pragma(inline, true) return Rectangle(0, 0,
-                sprite.texture.width, sprite.texture.height);
-    }
-
     public void render() {
         // draw the sprite
-        raylib.DrawTextureRec(sprite.texture, sprite_tex_rect(), entity.position2, raylib.WHITE);
+        raylib.DrawTextureRec(sprite.texture, sprite.src_rect, entity.position2, raylib.WHITE);
     }
 
     public void debug_render() {
-        auto tex_rec = sprite_tex_rect();
         raylib.DrawRectangleLines(cast(int) entity.position2.x,
-                cast(int) entity.position2.y, cast(int) tex_rec.width,
-                cast(int) tex_rec.height, raylib.RED);
+                cast(int) entity.position2.y, cast(int) sprite.src_rect.width,
+                cast(int) sprite.src_rect.height, raylib.RED);
     }
 }
