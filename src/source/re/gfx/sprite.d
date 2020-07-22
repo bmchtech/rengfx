58d3ecc7866b6aebbb28c9226a2a108fcb90f746
redthing1 <redthing1@alt.icu>
2020-07-22 15:54:07 -0700

    add origin to sprites

--- src/source/re/gfx/sprite.d
+++ src/source/re/gfx/sprite.d
@@ -6,10 +6,16 @@ import re.math;
 class Sprite {
     public Texture2D texture;
     public Rectangle src_rect;
+    public Vector2 origin;
 
-    this(Texture2D texture, Rectangle src_rect) {
+    this(Texture2D texture, Rectangle src_rect, Vector2 origin) {
         this.texture = texture;
         this.src_rect = src_rect;
+        this.origin = origin;
+    }
+
+    this(Texture2D texture, Rectangle src_rect) {
+        this(texture, src_rect, Vector2(src_rect.width / 2, src_rect.height / 2));
     }
 
     this(Texture2D texture) {
