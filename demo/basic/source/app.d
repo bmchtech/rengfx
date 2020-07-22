ddf7a47cb6c975960dac3aab10a66eccce472e0d
xdrie <xdrie@alt.icu>
2020-07-22 15:05:41 -0700

    reorganize basic

--- demo/basic/source/app.d
+++ demo/basic/source/app.d
@@ -1,5 +1,33 @@
 import std.stdio;
-import game;
+
+import re;
+import std.stdio;
+import bounce;
+
+class Game : Core {
+	this() {
+		super(960, 540, "basic demo");
+	}
+
+	override void initialize() {
+		super.initialize();
+
+		log.verbosity = Logger.Verbosity.Trace;
+		log.info("basic demo game initialized.");
+
+		content.base_path = "../content/";
+
+		// set up stuff and things
+		scenes ~= new BounceScene();
+
+		// set scene
+		scene = scenes[next_scene];
+		next_scene = (next_scene + 1) % scenes.length;
+	}
+
+	public static Scene[] scenes;
+	public static size_t next_scene = 0;
+}
 
 void main() {
 	auto game = new Game(); // init game
--- demo/basic/source/bounce.d
+++ demo/basic/source/bounce.d
@@ -1,26 +1,17 @@
-module play;
+module bounce;
 
 import re;
+import re.gfx;
 static import raylib;
-import re.gfx.sprite;
-import re.gfx.sprite_renderer;
 import re.math;
 
-class PlayScene : Scene {
-    this() {
-        clear_color = raylib.BLACK;
-    }
-
+class BounceScene : Scene {
     override void on_start() {
+        clear_color = raylib.BLACK;
+
         // add ball
         auto ball = create_entity("ball", Vector2(20, 20));
         auto ball_spr = new Sprite(Core.content.load_texture2d("ball.png"));
         ball.add_component(new SpriteRenderer(ball_spr));
-
-        Core.log.trace("play scene started.");
-    }
-
-    override void unload() {
-        Core.log.trace("play scene ended.");
     }
 }
