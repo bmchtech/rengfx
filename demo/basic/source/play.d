ab66f7b039a5a08b55d5ef4ffa924c7c69478d24
xdrie <xdrie@alt.icu>
2020-07-21 21:16:38 -0700

    add basic setup to demo

--- demo/basic/source/play.d
+++ demo/basic/source/play.d
@@ -2,6 +2,8 @@ module play;
 
 import re;
 static import raylib;
+import re.gfx.sprite;
+import re.gfx.sprite_renderer;
 
 class PlayScene : Scene {
     this() {
@@ -9,6 +11,11 @@ class PlayScene : Scene {
     }
 
     override void on_start() {
+        // add ball
+        auto ball = ecs.create_entity();
+        auto ball_spr = new Sprite(Core.content.load_texture2d("ball.png"));
+        ball.add_component(new SpriteRenderer(ball_spr));
+
         Core.log.info("play scene started.");
     }
 
