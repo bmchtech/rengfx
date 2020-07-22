8ab4859dc4696d286d1a2a78a30c0ddfa2af8a22
xdrie <xdrie@alt.icu>
2020-07-21 23:12:39 -0700

    support entity name and position

--- demo/basic/source/play.d
+++ demo/basic/source/play.d
@@ -13,8 +13,7 @@ class PlayScene : Scene {
 
     override void on_start() {
         // add ball
-        auto ball = ecs.create_entity();
-        ball.position = Vector2(20, 20);
+        auto ball = create_entity("ball", Vector2(20, 20));
         auto ball_spr = new Sprite(Core.content.load_texture2d("ball.png"));
         ball.add_component(new SpriteRenderer(ball_spr));
 
