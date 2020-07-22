2d65ebd3ae8bfa2bc8d22c06cd9d52d17eeceed5
redthing1 <redthing1@alt.icu>
2020-07-22 15:06:33 -0700

    make basic simpler

--- demo/basic/source/app.d
+++ demo/basic/source/app.d
@@ -12,21 +12,11 @@ class Game : Core {
 	override void initialize() {
 		super.initialize();
 
-		log.verbosity = Logger.Verbosity.Trace;
-		log.info("basic demo game initialized.");
-
+		log.info("hello, basic game!");
 		content.base_path = "../content/";
 
-		// set up stuff and things
-		scenes ~= new BounceScene();
-
-		// set scene
-		scene = scenes[next_scene];
-		next_scene = (next_scene + 1) % scenes.length;
+		scene = new BounceScene();
 	}
-
-	public static Scene[] scenes;
-	public static size_t next_scene = 0;
 }
 
 void main() {
