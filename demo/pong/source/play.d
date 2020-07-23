5134c6b61a9fe6d91459c4975ab671a3139271b8
xdrie <xdrie@alt.icu>
2020-07-23 02:09:56 -0700

    add simple bouncing

--- demo/pong/source/play.d
+++ demo/pong/source/play.d
@@ -28,11 +28,13 @@ class PlayScene : Scene {
         auto alice = create_entity("alice", Vector2(Core.window.width / 2, padding));
         alice.add_component(new SpriteRenderer(paddle_sprite));
         alice.add_component!LogicController();
-        player.add_component!Paddle();
+        alice.add_component!Paddle();
 
-        auto ball = create_entity("ball", Vector2(Core.window.width / 2, Core.window.height / 2));
-        ball.add_component(new SpriteRenderer(new Sprite(ball_tex)));
-        ball.add_component!Ball();
+        auto ball_nt = create_entity("ball", Vector2(Core.window.width / 2, Core.window.height / 2));
+        ball_nt.add_component(new SpriteRenderer(new Sprite(ball_tex)));
+        auto ball = ball_nt.add_component!Ball();
+        ball.bounce_on(player.get_component!Paddle());
+        ball.bounce_on(alice.get_component!Paddle());
 
         auto pong = create_entity("pong", Vector2(padding, Core.window.height / 2));
         auto pong_text = pong.add_component(new Text(Text.default_font, "pong", Text.default_size, raylib.WHITE));
