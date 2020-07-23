200c5df3e179fadc0c1687024591d476e49cf73f
redthing1 <redthing1@alt.icu>
2020-07-22 21:40:31 -0700

    move math imports

--- src/source/re/math/package.d
+++ src/source/re/math/package.d
@@ -4,4 +4,6 @@ public import re.math.transform;
 public import re.math.rect;
 
 // raylib
+public static import raymath;
+public import raylib : Vector2, Vector3, Matrix4;
 public import raylib: Rectangle;
