3b0868fb49a4fbcda81e4e8646a0eddedd2cd002
redthing1 <redthing1@alt.icu>
2020-07-22 21:47:36 -0700

    codedoc on entity

--- src/source/re/ecs/entity.d
+++ src/source/re/ecs/entity.d
@@ -9,32 +9,47 @@ import re.ecs.component;
 import re.ecs.storage;
 import re.math;
 
+/// a container for components
 class Entity {
+    /// the unique id of this entity
     public size_t id;
+    /// whether this entity is alive
     public bool alive;
+    /// world transform of entity
     public Transform transform;
+    /// friendly name
     public string name;
+    /// ecs manager
     public EntityManager manager;
+    /// list of component references
     public ComponentId[] components;
 
+    /// create entity given the ecs
     this(EntityManager manager) {
         this.manager = manager;
     }
 
+    /// prepare the instance as a fresh entity
     public void initialize() {
         alive = true;
+        transform = Transform();
+        name = string.init;
         components = [];
     }
 
+    /// release all resources and deinitialize
     public void destroy() {
         alive = false;
-        manager.storage.destroy_all(this);
+        manager.storage.destroy_all(this); // destroy all components
+        name = string.init;
     }
 
+    /// add an instance of a component given the type
     public T add_component(T)() {
         return add_component(new T());
     }
 
+    /// add a component
     public T add_component(T)(T component) {
         auto id = manager.storage.insert(this, component);
         components ~= id;
@@ -43,46 +58,51 @@ class Entity {
         return component;
     }
 
+    /// remove a matching component given a type
     public void remove_component(T)() {
         auto component = get_component!T();
         remove_component(component);
     }
 
+    /// remove a specific component
     public void remove_component(T)(T component) {
         manager.storage.remove(this, component);
     }
 
+    /// checks whether this entity contains a matching component
     public bool has_component(T)() {
-        // return components.data.any!(x => cast(T) x);
         return manager.storage.has_component!T(this);
     }
 
+    /// gets a matching component given a type
     public T get_component(T)() {
-        // auto i = components.data.countUntil!(x => cast(T) x !is null);
-        // assert(i < components.data.length,
-        //         "no matching component was found. use has_component() to ensure that the component exists.");
-        // return cast(T) components.data[i];
         return cast(T) manager.storage.get!T(this);
 
     }
 
+    /// gets all matching components given a type
     public T[] get_components(T)() {
         return manager.storage.get_all!T(this);
     }
 
     // - transform
+
+    /// forwards to transform
     @property Vector2 position2() {
         return transform.position2;
     }
 
+    /// forwards to transform
     @property Vector2 position2(Vector2 value) {
         return transform.position2 = value;
     }
 
+    /// forwards to transform
     @property Vector3 position() {
         return transform.position;
     }
 
+    /// forwards to transform
     @property Vector3 position(Vector3 value) {
         return transform.position = value;
     }
