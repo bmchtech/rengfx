52aaf3c9609616d1f2eeef84e1406990e0dd5fe6
redthing1 <redthing1@alt.icu>
2020-07-22 20:11:48 -0700

    create paddles

--- demo/pong/source/input.d
+++ demo/pong/source/input.d
@@ -3,15 +3,26 @@ module input;
 import re.ecs;
 import re.input;
 
-class InputComponent : Component {
+class InputController : Component {
     public VirtualAxis move;
 
     this() {
         move = new VirtualAxis();
-        move.nodes ~= new VirtualAxis.KeyboardKeys(Keys.KEY_LEFT, Keys.KEY_RIGHT);
     }
 
     public override void destroy() {
         move.unregister();
+    }
+}
+
+class PlayerController : InputController {
+    this() {
+        move.nodes ~= new VirtualAxis.KeyboardKeys(Keys.KEY_LEFT, Keys.KEY_RIGHT);
+    }
+}
+
+class LogicController : InputController {
+    this() {
+        move.nodes ~= new VirtualAxis.LogicAxis();
     }
 }
