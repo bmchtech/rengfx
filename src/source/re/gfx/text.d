5c3b1a6c8c2808e2a983737d7fe1536c1b37d036
redthing1 <redthing1@alt.icu>
2020-07-23 01:32:51 -0700

    add text align support

--- src/source/re/gfx/text.d
+++ src/source/re/gfx/text.d
@@ -20,7 +20,19 @@ class Text : Component, Renderable {
     private Vector2 _origin;
     /// default font size
     public enum default_size = 10;
-    
+    private Align _horiz_align;
+    private Align _vert_align;
+
+    /// alignment style
+    public enum Align {
+        /// left or top
+        Close,
+        /// center
+        Center,
+        /// right or bottom
+        Far
+    }
+
     /// create a new text
     this(raylib.Font font, string text, int size, raylib.Color color) {
         this.text = text;
@@ -46,8 +58,27 @@ class Text : Component, Renderable {
 
     private void update_dimens() {
         _text_size = raylib.MeasureTextEx(font, toStringz(text), size, spacing);
-        // TODO: support centering
-        _origin = Vector2(0, 0);
+        // calculate origin
+        auto ori_x = 0.0;
+        auto ori_y = 0.0;
+        if (_horiz_align == Align.Close)
+            ori_x = 0;
+        else if (_horiz_align == Align.Center)
+            ori_x = _text_size.x / 2;
+        else
+            ori_x = _text_size.x;
+        if (_vert_align == Align.Close)
+            ori_y = 0;
+        else if (_vert_align == Align.Center)
+            ori_y = _text_size.y / 2;
+        else
+            ori_y = _text_size.y;
+        _origin = Vector2(ori_x, ori_y);
+    }
+
+    public void set_align(Align horiz, Align vert) {
+        _horiz_align = horiz;
+        _vert_align = vert;
     }
 
     void render() {
