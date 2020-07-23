7cfefab4f27c0148b3af98acb824beb9327f62b5
redthing1 <redthing1@alt.icu>
2020-07-23 16:37:05 -0700

    add kinbody

---
+++ src/source/re/math/vector_ext.d
@@ -0,0 +1,19 @@
+module re.math.vector_ext;
+
+import re.math;
+
+// - raylib aliases
+alias Normalize = raymath.Vector2Normalize;
+alias Normalize = raymath.Vector3Normalize;
+
+// - custom
+
+float LengthSquared(Vector2 v) {
+    return (v.x * v.x) + (v.y * v.y);
+}
+
+
+enum Vector2Zero = Vector2(0, 0);
+enum Vector2One = Vector2(1, 1);
+enum Vector3Zero = Vector3(0, 0, 0);
+enum Vector3One = Vector3(1, 1, 1);
