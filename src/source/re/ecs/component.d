4534b9e1fdc7c616324651bee43a8cffbc09b1f5
xdrie <xdrie@alt.icu>
2020-07-23 16:17:11 -0700

    add transform forward to component

--- src/source/re/ecs/component.d
+++ src/source/re/ecs/component.d
@@ -1,15 +1,24 @@
 module re.ecs.component;
 
 import re.ecs.entity;
+import re.math.transform;
 
 /// the composable unit of functionality
 abstract class Component {
     /// owner entity
     public Entity entity;
+
+    /// initialize the component. entity is already set.
     public void setup() {
     }
 
+    /// release resources and clean up
     public void destroy() {
+    }
+
+    /// forward to entity.transform
+    @property public Transform transform() {
+        return entity.transform;
     }
 }
 
