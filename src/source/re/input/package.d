da67271ba9ff5a1158320580ce624e9042b68a0d
xdrie <xdrie@alt.icu>
2020-07-23 15:39:28 -0700

    add virtual joystick

--- src/source/re/input/package.d
+++ src/source/re/input/package.d
@@ -6,3 +6,4 @@ public import re.input.input;
 public import re.input.virtual.base;
 public import re.input.virtual.button;
 public import re.input.virtual.axis;
+public import re.input.virtual.joystick;
