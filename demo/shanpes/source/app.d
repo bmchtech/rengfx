b4f48848e0f003bf2761d883caa88fc99f4a4a7b
redthing1 <redthing1@alt.icu>
2020-07-23 14:53:31 -0700

    simple square

--- demo/shanpes/source/app.d
+++ demo/shanpes/source/app.d
@@ -1,15 +1,20 @@
 import std.stdio;
 
 import re;
+import re.math;
 import std.stdio;
 import play;
 
 class Game : Core {
+	enum WIDTH = 640;
+	enum HEIGHT = 480;
+
 	this() {
-		super(640, 480, "shanpes");
+		super(WIDTH, HEIGHT, "shanpes");
 	}
 
 	override void initialize() {
+		default_resolution = Vector2(WIDTH / 4, HEIGHT / 4);
 		content.paths ~= "../content/";
 
 		scene = new PlayScene();
--- demo/shanpes/source/comp/square.d
+++ demo/shanpes/source/comp/square.d
@@ -1,10 +1,26 @@
 module comp.square;
 
 import re.ecs;
+import re.math;
 static import raylib;
 
-class Square : Component {
-    this(raylib.Color color) {
+class Square : Component, Renderable {
+    public Vector2 size;
+    public raylib.Color color;
 
+    this(Vector2 size, raylib.Color color) {
+        this.size = size;
+        this.color = color;
+    }
+
+    @property Rectangle bounds() {
+        return Rectangle(entity.position2.x, entity.position2.y, size.x, size.y);
+    }
+
+    void render() {
+        raylib.DrawRectangleV(entity.position2, size, color);
+    }
+
+    void debug_render() {
     }
 }
