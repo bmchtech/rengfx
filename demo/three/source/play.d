a9fd7827c10d43524f147951afc77bd99ad8ad3b
redthing1 <redthing1@alt.icu>
2020-07-24 11:27:03 -0700

    create stub three project

---
+++ demo/three/source/play.d
@@ -0,0 +1,13 @@
+module play;
+
+import re;
+import re.gfx;
+import re.math;
+
+class PlayScene : Scene3D {
+    override void on_start() {
+        clear_color = Colors.LIGHTGRAY;
+        
+        // TODO: add scene entities
+    }
+}
