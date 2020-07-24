81f7c2173c2ec1a0dbf25842be1c715d7f78eb88
xdrie <xdrie@alt.icu>
2020-07-24 15:07:17 -0700

    move square -> ColorRect

--- demo/shanpes/source/play.d
+++ demo/shanpes/source/play.d
@@ -2,9 +2,9 @@ module play;
 
 import re;
 import re.gfx;
+import re.gfx.shapes.rect;
 import re.math;
 import comp.input;
-import comp.square;
 import comp.body;
 import comp.ai;
 static import raylib;
@@ -14,12 +14,12 @@ class PlayScene : Scene2D {
         clear_color = Colors.LIGHTGRAY;
 
         auto player = create_entity("player", Vector2(20, 20));
-        player.add_component(new Square(Vector2(8, 8), Colors.BLUE));
+        player.add_component(new ColorRect(Vector2(8, 8), Colors.BLUE));
         player.add_component!PlayerController();
         player.add_component!ShapeBody();
 
         auto turret = create_entity("turret", Vector2(80, 80));
-        turret.add_component(new Square(Vector2(8, 8), Colors.DARKGRAY));
+        turret.add_component(new ColorRect(Vector2(8, 8), Colors.DARKGRAY));
         turret.add_component!LogicController();
         turret.add_component!ShapeBody();
         turret.add_component!AiPlayer();
