3b0f9ec0c69787c1fceec58c2ec1653e1f83228b
redthing1 <redthing1@alt.icu>
2020-07-23 21:07:42 -0700

    separate out default commands

--- src/source/re/ng/command.d
+++ src/source/re/ng/command.d
@@ -1,8 +1,67 @@
 module re.ng.command;
 
+import re.core;
+import std.range;
+import std.array;
+import std.algorithm;
+import std.string;
+
 /// console commands
 struct ConsoleCommand {
     string name;
-    void delegate(string[]) action;
+    void function(string[]) action;
     string help;
+}
+
+static class DefaultCommands {
+    alias log = Core.log;
+    alias scene = Core.scene;
+    alias dbg = Core.debugger;
+
+    static void c_help(string[] args) {
+        auto sb = appender!string();
+        sb ~= "available commmands:\n";
+        auto command_names = dbg.commands.keys.sort();
+        foreach (command_name; command_names) {
+            auto command = dbg.commands[command_name];
+            sb ~= format("%s - %s\n", command.name, command.help);
+        }
+        log.info(sb.data);
+    }
+
+    static void c_entities(string[] args) {
+        auto sb = appender!string();
+        sb ~= "entities:\n";
+        foreach (entity; scene.ecs.entities) {
+            // get list of components
+            auto component_types = entity.get_all_components().map!(x => x.classinfo.name);
+            sb ~= format("%s: components[%d] {%s}\n", entity.name,
+                    entity.components.length, component_types);
+        }
+        log.info(sb.data);
+    }
+
+    static void c_inspect(string[] args) {
+        if (args.length < 2) {
+            log.err("usage: inspect <entity> <component>");
+            return;
+        }
+        auto nt_name = args[0];
+        if (!scene.ecs.has_entity(nt_name)) {
+            log.err(format("entity '%s' not found", nt_name));
+            return;
+        }
+        auto entity = scene.get_entity(nt_name);
+        auto comp_search = args[1].toLower;
+        // find matching component
+        auto matches = entity.get_all_components()
+            .find!(x => x.classinfo.name.toLower.indexOf(comp_search) > 0);
+        if (matches.length == 0) {
+            log.err(format("no matching component for '%s'", comp_search));
+            return;
+        }
+        auto comp = matches.front;
+        log.info(format("inspecting: %s", comp.classinfo.name));
+        // TODO: dump this component
+    }
 }
