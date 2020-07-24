b624321eb0216a61f68fc44d16ffbda52a22bfa9
xdrie <xdrie@alt.icu>
2020-07-24 11:27:03 -0700

    create stub three project

---
+++ demo/three/source/app.d
@@ -0,0 +1,30 @@
+module app;
+
+import std.stdio;
+
+import re;
+import re.math;
+import std.stdio;
+import play;
+
+class Game : Core {
+	enum WIDTH = 640;
+	enum HEIGHT = 480;
+
+	this() {
+		super(WIDTH, HEIGHT, "three");
+	}
+
+	override void initialize() {
+		default_resolution = Vector2(WIDTH / 4, HEIGHT / 4);
+		content.paths ~= "../content/";
+
+		scene = new PlayScene();
+	}
+}
+
+void main() {
+	auto game = new Game(); // init game
+	game.run();
+	game.destroy(); // clean up
+}
