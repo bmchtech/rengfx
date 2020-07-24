abef80d73dc533f4ae850958729afbe78e5ec318
xdrie <xdrie@alt.icu>
2020-07-23 18:08:41 -0700

    add ai

--- demo/shanpes/source/play.d
+++ demo/shanpes/source/play.d
@@ -6,6 +6,7 @@ import re.math;
 import comp.input;
 import comp.square;
 import comp.body;
+import comp.ai;
 static import raylib;
 
 class PlayScene : Scene {
@@ -13,8 +14,14 @@ class PlayScene : Scene {
         clear_color = Colors.LIGHTGRAY;
 
         auto player = create_entity("player", Vector2(20, 20));
-        player.add_component(new Square(Vector2(10, 10), Colors.BLUE));
+        player.add_component(new Square(Vector2(8, 8), Colors.BLUE));
         player.add_component!PlayerController();
         player.add_component!ShapeBody();
+
+        auto turret = create_entity("turret", Vector2(80, 80));
+        turret.add_component(new Square(Vector2(8, 8), Colors.DARKGRAY));
+        turret.add_component!LogicController();
+        turret.add_component!ShapeBody();
+        turret.add_component!AiPlayer();
     }
 }
