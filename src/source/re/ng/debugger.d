d2fb1a861455cbc1c711f001ea4172d0f709085c
xdrie <xdrie@alt.icu>
2020-07-23 19:15:11 -0700

    add stub debugger

--- src/source/re/ng/debugger.d
+++ src/source/re/ng/debugger.d
@@ -3,9 +3,14 @@ module re.ng.debugger;
 import re.core;
 import re.ecs.renderable;
 import re.input.input;
+import re.gfx;
 static import raylib;
 
 class Debugger {
+    private enum padding = 12;
+    private enum bg_col = Color(180, 180, 180, 180);
+    private enum console_height = 16;
+
     public void update() {
         if (Input.is_key_pressed(Keys.KEY_GRAVE)) {
             Core.debug_render = !Core.debug_render;
@@ -13,7 +18,8 @@ class Debugger {
     }
 
     public void render() {
-
+        // draw rect
+        raylib.DrawRectangle(padding, Core.window.height - padding - console_height, Core.window.width - padding * 2, console_height, bg_col);
     }
 
     public static void default_debug_render(Renderable renderable) {
