dd558c80916602af7d7a9b427263a9bc6beb5390
redthing1 <redthing1@alt.icu>
2020-07-24 11:38:08 -0700

    set up scene camera

--- demo/three/source/play.d
+++ demo/three/source/play.d
@@ -7,7 +7,13 @@ import re.math;
 class PlayScene : Scene3D {
     override void on_start() {
         clear_color = Colors.LIGHTGRAY;
-        
+
+        camera.position = Vector3(10, 10, 10);
+        camera.target = Vector3(0, 0, 0);
+        camera.up = Vector3(0, 1, 0);
+        camera.fovy = 45;
+        camera.type = CameraType.CAMERA_PERSPECTIVE;
+
         // TODO: add scene entities
     }
 }
