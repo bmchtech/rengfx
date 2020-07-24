5b3d4821b2f8c383aeecf58320cd0d9cb7e0a7d0
redthing1 <redthing1@alt.icu>
2020-07-24 11:35:14 -0700

    add default 3d camera

--- src/source/re/ng/scene3d.d
+++ src/source/re/ng/scene3d.d
@@ -8,7 +8,18 @@ import re.ecs;
 import re.math;
 
 abstract class Scene3D : Scene {
+    /// the 3d scene camera
+    public raylib.Camera3D camera;
+
+    override void begin() {
+        super.begin();
+
+        camera = raylib.Camera3D();
+    }
+
     override void render_scene() {
+        raylib.BeginMode3D(camera);
+
         // render 3d components
         foreach (component; ecs.storage.renderable_components) {
             auto renderable = cast(Renderable3D) component;
@@ -18,5 +29,7 @@ abstract class Scene3D : Scene {
                 renderable.debug_render();
             }
         }
+
+        raylib.EndMode3D();
     }
 }
