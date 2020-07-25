2961e2bde65d6c244e97591dc604f958cdab367c
xdrie <xdrie@alt.icu>
2020-07-25 12:19:32 -0700

    more time variables

--- src/source/re/time.d
+++ src/source/re/time.d
@@ -4,7 +4,21 @@ static import raylib;
 /// utility class for time calculations
 class Time {
     /// the time elapsed since the last update (a.k.a. dt)
-    static @property float delta_time() {
-        return raylib.GetFrameTime();
+    public static float delta_time = 0;
+    /// unscaled delta time
+    public static float raw_delta_time = 0;
+    /// total elapsed time
+    public static float total_time = 0;
+    /// time scale to apply to delta time
+    public static float time_scale = 1;
+    /// frame count
+    public static uint frame_count = 0;
+
+    /// internally used to update the time counters
+    public static void update(float dt) {
+        total_time += dt;
+        delta_time = dt * time_scale;
+        raw_delta_time = dt;
+        frame_count++;
     }
 }
