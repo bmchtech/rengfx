53794abe99b62a4addfc6ef7f6f85d872c0c40da
redthing1 <redthing1@alt.icu>
2020-07-24 23:13:11 -0700

    add postprocessor chain

--- src/source/re/gfx/postprocessor.d
+++ src/source/re/gfx/postprocessor.d
@@ -1,17 +1,25 @@
 module re.gfx.postprocessor;
 
 import re.gfx;
+import re.math;
 static import raylib;
 
+/// applies an effect to a rendertarget
 class PostProcessor {
+    /// the effect to apply to the input buffer
     public Effect effect;
+    /// the render target buffer of this postprocessor
+    public RenderTarget buffer;
 
-    this(Effect effect) {
+    this(Vector2 resolution, Effect effect) {
         this.effect = effect;
+
+        // create render target
+        buffer = raylib.LoadRenderTexture(cast(int) resolution.x, cast(int) resolution.y);
     }
 
-    public void process(RenderTarget source, RenderTarget dest) {
-        // TODO: draw src to dest
-        RenderExt.draw_render_target_from(source, dest, effect);
+    /// process the source and render to internal buffer
+    public void process(RenderTarget source) {
+        RenderExt.draw_render_target_from(source, buffer, effect);
     }
 }
