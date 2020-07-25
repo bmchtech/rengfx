67c1b11431fa6ef8a5ad7d75149de5072b50c3a2
redthing1 <redthing1@alt.icu>
2020-07-24 23:20:36 -0700

    add color utility functions

--- src/source/re/gfx/raytypes.d
+++ src/source/re/gfx/raytypes.d
@@ -9,3 +9,17 @@ public {
 
     alias RenderTarget = raylib.RenderTexture2D;
 }
+
+// - utility functions
+
+pragma(inline) {
+    /// gets a color that is white with a given alpha
+    public Color color_alpha_white(float alpha) {
+        return raylib.ColorFromNormalized(raylib.Vector4(1, 1, 1, alpha));
+    }
+
+    /// gets a color from floats
+    public Color color_alpha_white(float r, float g, float b, float a = 1) {
+        return raylib.ColorFromNormalized(raylib.Vector4(r, g, b, a));
+    }
+}
