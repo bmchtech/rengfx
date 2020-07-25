d3401303a299b1e9a2c6e6604540c6ee674c9dde
redthing1 <redthing1@alt.icu>
2020-07-25 01:25:53 -0700

    support more shader vars

--- src/source/re/gfx/effect.d
+++ src/source/re/gfx/effect.d
@@ -29,6 +29,18 @@ struct Effect {
         } else static if (is(T == Vector3)) {
             val_type = vartype.UNIFORM_VEC3;
             float[3] data = [value.x, value.y, value.z];
+        } else static if (is(T == Vector4)) {
+            val_type = vartype.UNIFORM_VEC4;
+            float[4] data = [value.x, value.y, value.z, value.w];
+        } else static if (is(T == int[2])) {
+            val_type = vartype.UNIFORM_IVEC2;
+            int[2] data = [value[0], value[1]];
+        } else static if (is(T == int[3])) {
+            val_type = vartype.UNIFORM_IVEC3;
+            int[3] data = [value[0], value[1], value[2]];
+        } else static if (is(T == int[4])) {
+            val_type = vartype.UNIFORM_IVEC4;
+            int[4] data = [value[0], value[1], value[2], value[3]];
         } else {
             static assert(0, "unrecognized shader value data type");
         }
