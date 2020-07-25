84c614b259610de99dfd2098824a43bb2c9993f7
xdrie <xdrie@alt.icu>
2020-07-24 19:34:28 -0700

    temp disable transformation for repro bug

--- src/source/re/math/rect_ext.d
+++ src/source/re/math/rect_ext.d
@@ -17,26 +17,27 @@ static class RectangleExt {
         } else {
             auto tmp1 = Matrix4.init;
 
-            // set the reference point to world reference taking origin into account
-            auto transform_mat = raymath.MatrixTranslate(-position.x - origin.x, -position.y - origin.y, 0);
-            tmp1 = raymath.MatrixScale(scale.x, scale.y, 1); // scale ->
-            transform_mat = raymath.MatrixMultiply(transform_mat, tmp1);
-            tmp1 = raymath.MatrixRotateZ(rotation); // rotate ->
-            transform_mat = raymath.MatrixMultiply(transform_mat, tmp1);
-            tmp1 = raymath.MatrixTranslate(position.x, position.y, 0); // translate back
-            transform_mat = raymath.MatrixMultiply(transform_mat, tmp1);
+            auto transform_mat = raymath.MatrixIdentity();
+            // // set the reference point to world reference taking origin into account
+            // auto transform_mat = raymath.MatrixTranslate(-position.x - origin.x, -position.y - origin.y, 0);
+            // tmp1 = raymath.MatrixScale(scale.x, scale.y, 1); // scale ->
+            // transform_mat = raymath.MatrixMultiply(transform_mat, tmp1);
+            // tmp1 = raymath.MatrixRotateZ(rotation); // rotate ->
+            // transform_mat = raymath.MatrixMultiply(transform_mat, tmp1);
+            // tmp1 = raymath.MatrixTranslate(position.x, position.y, 0); // translate back
+            // transform_mat = raymath.MatrixMultiply(transform_mat, tmp1);
 
             // get all four corners in world space
-            auto top_left = Vector3(position.x, position.y, 0);
-            auto top_right = Vector3(position.x + width, position.y, 0);
-            auto btm_left = Vector3(position.x, position.y + height, 0);
-            auto btm_right = Vector3(position.x + width, position.y + height, 0);
+            auto top_left_w = Vector3(position.x, position.y, 0);
+            auto top_right_w = Vector3(position.x + width, position.y, 0);
+            auto btm_left_w = Vector3(position.x, position.y + height, 0);
+            auto btm_right_w = Vector3(position.x + width, position.y + height, 0);
 
             // transform the corners into our work space
-            top_left = raymath.Vector3Transform(top_left, transform_mat);
-            top_right = raymath.Vector3Transform(top_right, transform_mat);
-            btm_left = raymath.Vector3Transform(btm_left, transform_mat);
-            btm_right = raymath.Vector3Transform(btm_right, transform_mat);
+            auto top_left = raymath.Vector3Transform(top_left_w, transform_mat);
+            auto top_right = raymath.Vector3Transform(top_right_w, transform_mat);
+            auto btm_left = raymath.Vector3Transform(btm_left_w, transform_mat);
+            auto btm_right = raymath.Vector3Transform(btm_right_w, transform_mat);
 
             // find the min and max values so we can concoct our bounding box
             auto min_x = cast(int) min(top_left.x, btm_right.x, top_right.x, btm_left.x);
