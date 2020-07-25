2e644d33baa18e77986dd9fc6ed6c7b8cb90b38d
xdrie <xdrie@alt.icu>
2020-07-24 23:24:17 -0700

    add color_fade and color_hsv

--- src/source/re/gfx/raytypes.d
+++ src/source/re/gfx/raytypes.d
@@ -22,4 +22,14 @@ pragma(inline) {
     public Color color_alpha_white(float r, float g, float b, float a = 1) {
         return raylib.ColorFromNormalized(raylib.Vector4(r, g, b, a));
     }
+
+    /// fades a color
+    public Color color_fade(Color color, float fade) {
+        return raylib.Fade(color, fade);
+    }
+
+    /// gets a color from hsv
+    public Color color_hsv(float h, float s, float v) {
+        return raylib.ColorFromHSV(h, s, v);
+    }
 }
