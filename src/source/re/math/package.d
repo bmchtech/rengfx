54fb2ccf46729c204287121131b4bde8c293b113
xdrie <xdrie@alt.icu>
2020-07-24 18:13:41 -0700

    float constants

--- src/source/re/math/package.d
+++ src/source/re/math/package.d
@@ -8,15 +8,15 @@ public {
     import std.math;
 
     /// the mathematical constant pi
-    enum C_PI = std.math.PI;
-    enum C_PI_2 = std.math.PI / 2;
-    enum C_PI_4 = std.math.PI / 4;
+    enum C_PI = cast(float) std.math.PI;
+    enum C_PI_2 = cast(float) std.math.PI / 2;
+    enum C_PI_4 = cast(float) std.math.PI / 4;
 
     /// factor to convert radians to degrees
-    enum C_RAD2DEG = (180.0f / PI);
+    enum C_RAD2DEG = cast(float)(180.0 / PI);
 
     /// factor to convert degrees to radians
-    enum C_DEG2RAD = (PI / 180.0f);
+    enum C_DEG2RAD = cast(float)(PI / 180.0);
 
     // raylib
     static import raymath;
