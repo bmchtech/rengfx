dc0c762438b54015eaf36d7e6b07e00441a82a62
xdrie <xdrie@alt.icu>
2020-07-25 21:31:19 -0700

    more comments in three demo

--- demo/three/source/play.d
+++ demo/three/source/play.d
@@ -8,6 +8,7 @@ import re.ng.camera;
 import re.math;
 static import raylib;
 
+/// simple 3d demo scene
 class PlayScene : Scene3D {
     private PostProcessor glitch_postproc;
     private float[2] aberrationOffset = [0.01, 0];
@@ -15,19 +16,21 @@ class PlayScene : Scene3D {
     override void on_start() {
         clear_color = Colors.LIGHTGRAY;
 
-        // load the effect and add it as a postprocessor
+        // load a shader effect and add it as a postprocessor
         auto chrm_abr = Effect(Core.content.load_shader(null,
                 "chromatic_aberration.frag"), color_alpha_white(0.8));
         glitch_postproc = new PostProcessor(resolution, chrm_abr);
         glitch_postproc.enabled = false;
         postprocessors ~= glitch_postproc;
 
+        // set the camera position
         cam.entity.position = Vector3(0, 10, 10);
-        cam.look_at(Vector3(0, 0, 0));
 
         auto block = create_entity("block", Vector3(0, 0, 0));
         auto cube = block.add_component(new Cube(Vector3(2, 2, 2)));
 
+        // point the camera at the block, then orbit it
+        cam.look_at(block.position);
         cam.entity.add_component(new CameraOrbit(block, 0.5));
 
         // enable an example shader on cube
@@ -37,6 +40,7 @@ class PlayScene : Scene3D {
         cross_stitch.set_shader_var("mixAmt", mixAmt);
         cube.effect = cross_stitch;
 
+        // draw a grid at the origin
         auto grid = create_entity("grid");
         grid.add_component(new Grid3D(10, 1));
     }
@@ -44,11 +48,12 @@ class PlayScene : Scene3D {
     override void update() {
         super.update();
 
+        // allow the postprocessor to be toggled with SPACE
         if (Input.is_key_pressed(Keys.KEY_SPACE)) {
             glitch_postproc.enabled = !glitch_postproc.enabled;
         }
 
-        // make our postprocess effect fluctuate
+        // make our postprocess effect fluctuate with time
         import std.math : sin;
 
         aberrationOffset[0] = 0.010 + 0.005 * sin(Time.total_time / 2);
