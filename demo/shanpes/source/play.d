152c5b4bb07e07eb8489570ad33d332e9f2b0864
redthing1 <redthing1@alt.icu>
2020-07-25 19:20:04 -0700

    support multi-variable tweening

--- demo/shanpes/source/play.d
+++ demo/shanpes/source/play.d
@@ -11,7 +11,8 @@ static import raylib;
 
 class PlayScene : Scene2D {
     override void on_start() {
-        Tweener.tween(clear_color, Colors.DARKGRAY, Colors.LIGHTGRAY, 2, &Ease.QuadIn);
+        auto bg_tween = Tweener.tween(clear_color, Colors.DARKGRAY, Colors.LIGHTGRAY, 2, &Ease.QuadIn);
+        bg_tween.start();
 
         auto player = create_entity("player", Vector2(20, 20));
         player.add_component(new ColorRect(Vector2(8, 8), Colors.BLUE));
@@ -24,6 +25,7 @@ class PlayScene : Scene2D {
         turret.add_component!ShapeBody();
         turret.add_component!AiPlayer();
 
-        Tweener.tween(turret.position.x, 80, 20, 4, &Ease.QuadIn, 2);
+        auto turret_tween = Tweener.tween(turret.position.x, 80, 20, 4, &Ease.QuadInOut, 2);
+        turret_tween.start();
     }
 }
