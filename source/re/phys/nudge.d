bbbf6f89f6e17a43d6492ad95ae0deca2e190a78
redthing1 <redthing1@alt.icu>
2020-07-26 22:15:22 -0700

    create stub NudgeBody

---
+++ source/re/phys/nudge.d
@@ -0,0 +1,12 @@
+module re.phys.nudge;
+
+import re.ecs.component;
+import re.ecs.updatable;
+import re.math;
+import std.math;
+import re.time;
+
+class NudgeBody : Component, Updatable {
+    void update() {
+    }
+}
