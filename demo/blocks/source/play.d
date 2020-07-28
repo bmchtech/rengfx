fb60e5b52a3ed53fa0b0c9ca466cb3ad5d59339b
redthing1 <redthing1@alt.icu>
2020-07-28 02:26:57 -0700

    make small blocks

--- demo/blocks/source/play.d
+++ demo/blocks/source/play.d
@@ -21,7 +21,7 @@ class PlayScene : Scene3D {
         // draw a grid at the origin
         auto grid = create_entity("grid");
         grid.add_component(new Grid3D(20, 1));
-        
+
         auto floor = create_entity("floor", Vector3(0, -10, 0));
         floor.add_component(new Cube(Vector3(40, 4, 40), Colors.GRAY));
         floor.add_component(new BoxCollider(Vector3(400, 10, 400), Vector3(0, -10, 0)));
@@ -34,6 +34,17 @@ class PlayScene : Scene3D {
         block.add_component(new BoxCollider(Vector3(2, 2, 2), Vector3Zero));
         auto block_body = block.add_component(new NudgeBody());
         block_body.is_static = true;
+
+        // make small blocks
+        for (int i = 0; i < 20; i++) {
+            auto x_off = Rng.next_float() * 6;
+            auto z_off = Rng.next_float() * 6;
+
+            auto nt = create_entity("thing", Vector3(x_off, 40, z_off));
+            nt.add_component(new Cube(Vector3(0.5, 0.5, 0.5), Colors.GREEN));
+            nt.add_component(new BoxCollider(Vector3(0.5, 0.5, 0.5), Vector3Zero));
+            auto thing_body = nt.add_component(new NudgeBody());
+        }
 
         // point the camera at the block, then orbit it
         cam.look_at(block);
