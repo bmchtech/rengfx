8f926675f38544031c30d67173bab94dbdceebb8
xdrie <xdrie@alt.icu>
2020-07-28 01:42:40 -0700

    shronk

--- demo/blocks/source/play.d
+++ demo/blocks/source/play.d
@@ -23,13 +23,13 @@ class PlayScene : Scene3D {
         grid.add_component(new Grid3D(20, 1));
         
         auto floor = create_entity("floor", Vector3(0, -10, 0));
-        floor.add_component(new Cube(Vector3(40, 10, 40), Colors.GRAY));
+        floor.add_component(new Cube(Vector3(40, 4, 40), Colors.GRAY));
         floor.add_component(new BoxCollider(Vector3(400, 10, 400), Vector3(0, -10, 0)));
         auto floor_body = floor.add_component(new NudgeBody());
         floor_body.is_static = true;
 
         // create a block, and assign it a physics object
-        auto block = create_entity("block", Vector3(0, 0, 0));
+        auto block = create_entity("block", Vector3(0, 10, 0));
         block.add_component(new Cube(Vector3(2, 2, 2), Colors.BLUE));
         block.add_component(new BoxCollider(Vector3(2, 2, 2), Vector3Zero));
         block.add_component(new NudgeBody());
