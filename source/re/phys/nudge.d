d8e478ef2d630e7885a9c01e908546bf07dd7f15
redthing1 <redthing1@alt.icu>
2020-07-27 17:10:59 -0700

    add stub methods for registering physics bodies

--- source/re/phys/nudge.d
+++ source/re/phys/nudge.d
@@ -18,6 +18,10 @@ version (physics) {
         private NudgeRealm realm;
         private uint item_limit = 1024;
 
+        /// map from NudgeBody to physics body index
+        private uint[NudgeBody] _body_map;
+        private NudgeBody[uint] _body_map_reverse;
+
         /// checks whether this scene has a nudge manager installed
         public static bool is_installed(Scene scene) {
             auto existing = scene.get_manager!NudgeManager();
@@ -56,19 +60,61 @@ version (physics) {
             simulate();
         }
 
-        public void register(NudgeBody body) {
-            // TODO: register this body
+        /// registers a body
+        public void register(NudgeBody body_comp) {
+            auto inertia_inverse = 1 / body_comp.inertia;
+            nudge.BodyProperties properties = nudge.BodyProperties([
+                    inertia_inverse, inertia_inverse, inertia_inverse
+                    ], 1 / body_comp.mass);
+
+            // request a body from nudge
+            immutable auto body_id = realm.append_body(NudgeRealm.identity_transform,
+                    properties, NudgeRealm.zero_momentum);
+
+            body_comp.nudge_body_id = body_id;
+            body_comp.physics_synced = true;
+
+            // store in map
+            _body_map[body_comp] = body_id;
         }
 
-        public void unregister(NudgeBody body) {
-            // TODO: unregister this body
+        /// unregisters a body
+        public void unregister(NudgeBody body_comp) {
+            auto body_id = body_comp.nudge_body_id;
+
+            // swap indices in realm
+
+
+            // mark body as unsynced
+            body_comp.nudge_body_id = 0;
+            body_comp.physics_synced = false;
+        }
+
+        /// used to sync a body's properties with the physics system when they change
+        public void refresh(NudgeBody body_comp) {
+            auto body_id = body_comp.nudge_body_id;
+            // update mass
+            realm.bodies.properties[body_id].mass_inverse = (1 / body_comp.mass);
+
+            // update inertia
+            auto inertia_inverse = 1 / body_comp.inertia;
+            realm.bodies.properties[body_id].inertia_inverse = [
+                inertia_inverse, inertia_inverse, inertia_inverse
+            ];
         }
     }
 
     /// represents a physics body that uses the nudge physics system
     class NudgeBody : Component, Updatable {
         public uint nudge_body_id;
+
+        /// whether this body is currently in sync with the physics system
+        public bool physics_synced = false;
+
         private NudgeManager mgr;
+
+        private float _mass;
+        private float _inertia;
 
         override void setup() {
             // ensure the nudge system is installed
@@ -80,6 +126,30 @@ version (physics) {
 
             // register with nudge
             mgr.register(this);
+        }
+
+        /// gets the body's mass
+        @property public float mass() {
+            return _mass;
+        }
+
+        /// sets the body's mass
+        @property public float mass(float value) {
+            _mass = value;
+            mgr.refresh(this);
+            return value;
+        }
+
+        /// gets the body's moment of inertia
+        @property public float inertia() {
+            return _inertia;
+        }
+
+        /// sets the body's moment of inertia
+        @property public float inertia(float value) {
+            _inertia = value;
+            mgr.refresh(this);
+            return _inertia;
         }
 
         void update() {
