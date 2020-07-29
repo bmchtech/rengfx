462e726fd04019a9294eed5822a358316e1ebd7d
redthing1 <redthing1@alt.icu>
2020-07-28 22:40:19 -0700

    add more small bodies

--- demo/blocks/source/play.d
+++ demo/blocks/source/play.d
@@ -16,33 +16,36 @@ class PlayScene : Scene3D {
         clear_color = Colors.LIGHTGRAY;
 
         // set the camera position
-        cam.entity.position = Vector3(0, 30, 20);
+        cam.entity.position = Vector3(0, 10, 20);
 
         // draw a grid at the origin
         auto grid = create_entity("grid");
         grid.add_component(new Grid3D(20, 1));
 
-        auto floor = create_entity("floor", Vector3(0, -10, 0));
-        floor.add_component(new Cube(Vector3(40, 4, 40), Colors.GRAY));
-        floor.add_component(new BoxCollider(Vector3(400, 10, 400), Vector3(0, -10, 0)));
+        auto floor = create_entity("floor", Vector3(0, -5, 0));
+        floor.add_component(new Cube(Vector3(40, 10, 40), Colors.GRAY));
+        floor.add_component(new BoxCollider(Vector3(40, 5, 40), Vector3Zero));
         auto floor_body = floor.add_component(new StaticBody());
 
         // create a block, and assign it a physics object
-        auto block = create_entity("block", Vector3(0, 20, 0));
-        block.add_component(new Cube(Vector3(2, 2, 2), Colors.BLUE));
+        auto block = create_entity("block", Vector3(0, 5, 0));
+        block.add_component(new Cube(Vector3(4, 4, 4), Colors.BLUE));
         block.add_component(new BoxCollider(Vector3(2, 2, 2), Vector3Zero));
         auto block_body = block.add_component(new DynamicBody());
 
-        // // make small blocks
-        // for (int i = 0; i < 20; i++) {
-        //     auto x_off = Rng.next_float() * 6;
-        //     auto z_off = Rng.next_float() * 6;
+        // make small blocks
+        enum small_block_count = 200;
+        enum small_block_spread = 10;
+        for (int i = 0; i < small_block_count; i++) {
+            auto x_off = Rng.next_float() * small_block_spread * 2 - small_block_spread;
+            auto y_off = Rng.next_float() * small_block_spread * 4;
+            auto z_off = Rng.next_float() * small_block_spread * 2 - small_block_spread;
 
-        //     auto nt = create_entity("thing", Vector3(x_off, 40, z_off));
-        //     nt.add_component(new Cube(Vector3(0.5, 0.5, 0.5), Colors.GREEN));
-        //     nt.add_component(new BoxCollider(Vector3(0.5, 0.5, 0.5), Vector3Zero));
-        //     auto thing_body = nt.add_component(new DynamicBody());
-        // }
+            auto nt = create_entity("thing", Vector3(x_off, y_off, z_off));
+            nt.add_component(new Cube(Vector3(1, 1, 1), Colors.GREEN));
+            nt.add_component(new BoxCollider(Vector3(0.5, 0.5, 0.5), Vector3Zero));
+            auto thing_body = nt.add_component(new DynamicBody());
+        }
 
         // point the camera at the block, then orbit it
         cam.look_at(block);
