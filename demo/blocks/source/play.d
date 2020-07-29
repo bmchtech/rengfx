e39ffdc8e41c90ac64172378fdd648ce43eaec48
redthing1 <redthing1@alt.icu>
2020-07-29 00:22:02 -0700

    set main cube to random orientation

--- demo/blocks/source/play.d
+++ demo/blocks/source/play.d
@@ -27,32 +27,37 @@ class PlayScene : Scene3D {
         floor.add_component(new BoxCollider(Vector3(40, 5, 40), Vector3Zero));
         auto floor_body = floor.add_component(new StaticBody());
 
+        auto x_ang = Rng.next_float() * C_2_PI;
+        auto y_ang = Rng.next_float() * C_2_PI;
+        auto z_ang = Rng.next_float() * C_2_PI;
+
         // create a block, and assign it a physics object
         auto block = create_entity("block", Vector3(0, 5, 0));
+        block.transform.orientation = raymath.QuaternionFromEuler(x_ang, y_ang, z_ang);
         block.add_component(new Cube(Vector3(4, 4, 4), Colors.BLUE));
         block.add_component(new BoxCollider(Vector3(2, 2, 2), Vector3Zero));
         auto block_body = block.add_component(new DynamicBody());
 
-        // make small blocks
-        enum small_block_count = 3;
-        enum small_block_spread = 10;
-        for (int i = 0; i < small_block_count; i++) {
-            auto x_off = Rng.next_float() * small_block_spread * 2 - small_block_spread;
-            auto y_off = Rng.next_float() * small_block_spread * 4;
-            auto z_off = Rng.next_float() * small_block_spread * 2 - small_block_spread;
+        // // make small blocks
+        // enum small_block_count = 1;
+        // enum small_block_spread = 10;
+        // for (int i = 0; i < small_block_count; i++) {
+        //     auto x_off = Rng.next_float() * small_block_spread * 2 - small_block_spread;
+        //     auto y_off = Rng.next_float() * small_block_spread * 4;
+        //     auto z_off = Rng.next_float() * small_block_spread * 2 - small_block_spread;
 
-            auto x_ang = Rng.next_float() * C_2_PI;
-            auto y_ang = Rng.next_float() * C_2_PI;
-            auto z_ang = Rng.next_float() * C_2_PI;
+        //     auto x_ang = Rng.next_float() * C_2_PI;
+        //     auto y_ang = Rng.next_float() * C_2_PI;
+        //     auto z_ang = Rng.next_float() * C_2_PI;
 
-            static import raymath;
+        //     static import raymath;
 
-            auto nt = create_entity("thing", Vector3(x_off, y_off, z_off));
-            nt.transform.orientation = raymath.QuaternionFromEuler(x_ang, y_ang, z_ang);
-            nt.add_component(new Cube(Vector3(1, 1, 1), Colors.GREEN));
-            nt.add_component(new BoxCollider(Vector3(0.5, 0.5, 0.5), Vector3Zero));
-            auto thing_body = nt.add_component(new DynamicBody());
-        }
+        //     auto nt = create_entity("thing", Vector3(x_off, y_off, z_off));
+        //     nt.transform.orientation = raymath.QuaternionFromEuler(x_ang, y_ang, z_ang);
+        //     nt.add_component(new Cube(Vector3(1, 1, 1), Colors.GREEN));
+        //     nt.add_component(new BoxCollider(Vector3(0.5, 0.5, 0.5), Vector3Zero));
+        //     auto thing_body = nt.add_component(new DynamicBody());
+        // }
 
         // point the camera at the block, then orbit it
         cam.look_at(block);
