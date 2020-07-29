9f6d85c24291c09a0e31829b5d31c01644c16047
redthing1 <redthing1@alt.icu>
2020-07-28 19:36:51 -0700

    NudgeBody -> RigidBody in demo

--- demo/blocks/source/play.d
+++ demo/blocks/source/play.d
@@ -6,7 +6,7 @@ import re.gfx.shapes.cube;
 import re.gfx.shapes.grid;
 import re.ng.camera;
 import re.phys.collider;
-import re.phys.nudge;
+import re.phys.rigid3d;
 import re.math;
 static import raylib;
 
@@ -25,26 +25,26 @@ class PlayScene : Scene3D {
         auto floor = create_entity("floor", Vector3(0, -10, 0));
         floor.add_component(new Cube(Vector3(40, 4, 40), Colors.GRAY));
         floor.add_component(new BoxCollider(Vector3(400, 10, 400), Vector3(0, -10, 0)));
-        auto floor_body = floor.add_component(new NudgeBody());
+        auto floor_body = floor.add_component(new RigidBody());
         floor_body.is_static = true;
 
         // create a block, and assign it a physics object
         auto block = create_entity("block", Vector3(0, 20, 0));
         block.add_component(new Cube(Vector3(2, 2, 2), Colors.BLUE));
         block.add_component(new BoxCollider(Vector3(2, 2, 2), Vector3Zero));
-        auto block_body = block.add_component(new NudgeBody());
+        auto block_body = block.add_component(new RigidBody());
         block_body.is_static = true;
 
-        // make small blocks
-        for (int i = 0; i < 20; i++) {
-            auto x_off = Rng.next_float() * 6;
-            auto z_off = Rng.next_float() * 6;
+        // // make small blocks
+        // for (int i = 0; i < 20; i++) {
+        //     auto x_off = Rng.next_float() * 6;
+        //     auto z_off = Rng.next_float() * 6;
 
-            auto nt = create_entity("thing", Vector3(x_off, 40, z_off));
-            nt.add_component(new Cube(Vector3(0.5, 0.5, 0.5), Colors.GREEN));
-            nt.add_component(new BoxCollider(Vector3(0.5, 0.5, 0.5), Vector3Zero));
-            auto thing_body = nt.add_component(new NudgeBody());
-        }
+        //     auto nt = create_entity("thing", Vector3(x_off, 40, z_off));
+        //     nt.add_component(new Cube(Vector3(0.5, 0.5, 0.5), Colors.GREEN));
+        //     nt.add_component(new BoxCollider(Vector3(0.5, 0.5, 0.5), Vector3Zero));
+        //     auto thing_body = nt.add_component(new RigidBody());
+        // }
 
         // point the camera at the block, then orbit it
         cam.look_at(block);
