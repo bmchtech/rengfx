6298250304099006549767831eec517c6bdee842
redthing1 <redthing1@alt.icu>
2020-07-30 16:09:39 -0700

    allow turn and roll

--- demo/blocks/source/comp/body.d
+++ demo/blocks/source/comp/body.d
@@ -9,6 +9,8 @@ static import raymath;
 class Character : Component, Updatable {
     /// movement speed
     enum move_speed = 20;
+    /// turning speed
+    enum turn_speed = 10;
 
     private InputController controller;
     private DynamicBody bod;
@@ -17,7 +19,7 @@ class Character : Component, Updatable {
         controller = entity.get_component!InputController();
         bod = entity.get_component!DynamicBody();
 
-        bod.max_speed  = move_speed;
+        bod.max_speed = move_speed;
     }
 
     override void update() {
@@ -29,6 +31,11 @@ class Character : Component, Updatable {
         if (raymath.Vector2LengthSqr(controller.move.value) > 0) {
             bod.apply_impulse(Vector3(controller.move.value.x * move_speed, 0,
                     controller.move.value.y * move_speed), Vector3Zero);
+        }
+
+        if (raymath.Vector2LengthSqr(controller.turn.value) > 0) {
+            bod.apply_torque(Vector3(controller.turn.value.x * turn_speed * bod.mass,
+                    0, controller.turn.value.y * turn_speed * bod.mass));
         }
     }
 }
--- demo/blocks/source/comp/input.d
+++ demo/blocks/source/comp/input.d
@@ -6,10 +6,12 @@ import re.math;
 
 abstract class InputController : Component {
     public VirtualJoystick move;
+    public VirtualJoystick turn;
     public VirtualButton jump;
 
     this() {
         move = new VirtualJoystick();
+        turn = new VirtualJoystick();
         jump = new VirtualButton();
     }
 
@@ -20,7 +22,9 @@ abstract class InputController : Component {
 
 class PlayerController : InputController {
     this() {
-        move.nodes ~= new VirtualJoystick.KeyboardKeys(Keys.KEY_LEFT,
+        move.nodes ~= new VirtualJoystick.KeyboardKeys(Keys.KEY_A, Keys.KEY_D,
+                Keys.KEY_W, Keys.KEY_S);
+        turn.nodes ~= new VirtualJoystick.KeyboardKeys(Keys.KEY_LEFT,
                 Keys.KEY_RIGHT, Keys.KEY_UP, Keys.KEY_DOWN);
         jump.nodes ~= new VirtualButton.KeyboardKey(Keys.KEY_SPACE);
     }
