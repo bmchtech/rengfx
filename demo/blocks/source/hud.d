7f256522f9a0d80f4cdf15bab81b0bfa71cd4b4b
xdrie <xdrie@alt.icu>
2020-07-30 16:20:13 -0700

    add controls to hud

--- demo/blocks/source/hud.d
+++ demo/blocks/source/hud.d
@@ -13,12 +13,18 @@ class HUDScene : Scene2D {
 
         enum pad = 4;
 
-        auto msg = create_entity("msg", Vector2(pad, resolution.y - pad));
-        auto hello_text = msg.add_component(new Text(Text.default_font,
-                "blocks - physics and lighting", 20, Colors.BROWN));
+        auto desc = create_entity("desc", Vector2(pad, resolution.y - pad));
+        auto hello_text = desc.add_component(new Text(Text.default_font,
+                "blocks - physics and lighting", 30, Colors.BROWN));
         hello_text.set_align(Text.Align.Close, Text.Align.Far);
 
         auto diag = create_entity("diag", Vector2(2, 2));
         diag.add_component!FPSCounter();
+
+        auto instr = create_entity("instr", Vector2(resolution.x - pad * 4, pad * 4));
+        auto instr_text = instr.add_component(new Text(Text.default_font,
+                "-- Controls --\nSPACE - jump\nWASD - move\nARROW - turn\n" ~ "SCROLL - zoom\nRMB - orbit",
+                20, Colors.BROWN));
+        instr_text.set_align(Text.Align.Far, Text.Align.Close);
     }
 }
