5baa58aa6b19abfc09eba82932fae2f80fa82232
xdrie <xdrie@alt.icu>
2020-07-29 23:40:08 -0700

    refactor into SceneLights

--- source/re/ng/manager.d
+++ source/re/ng/manager.d
@@ -1,7 +1,11 @@
 module re.ng.manager;
 
+import re.ng.scene;
+
 /// represents a global engine system
 abstract class Manager {
+    /// set when attached to a scene
+    public Scene scene;
     /// updates this manager
     void update() {
     }
