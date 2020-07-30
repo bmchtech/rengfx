765c56253b0446b8409a12bc7f3c536c7ceafb23
xdrie <xdrie@alt.icu>
2020-07-29 18:22:35 -0700

    support basic movement

--- demo/blocks/source/comp/body.d
+++ demo/blocks/source/comp/body.d
@@ -4,10 +4,11 @@ import re;
 import re.math;
 import comp.input;
 import re.phys.rigid3d;
+static import raymath;
 
 class Character : Component, Updatable {
     /// movement speed
-    enum move_speed = 40;
+    enum move_speed = 10;
 
     private InputController controller;
     private DynamicBody bod;
@@ -20,7 +21,12 @@ class Character : Component, Updatable {
     override void update() {
         if (controller.jump.is_pressed) {
             // jump
-            bod.apply_impulse(Vector3(0, 128, 0), Vector3Zero);
+            bod.apply_impulse(Vector3(0, bod.mass * 4, 0), Vector3Zero);
+        }
+
+        if (raymath.Vector2LengthSqr(controller.move.value) > 0) {
+            bod.apply_impulse(Vector3(controller.move.value.x * move_speed, 0,
+                    controller.move.value.y * move_speed), Vector3Zero);
         }
     }
 }
