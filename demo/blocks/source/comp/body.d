5ee354fe96a7ea4eded4fd013137e0855608345e
xdrie <xdrie@alt.icu>
2020-07-30 16:47:39 -0700

    use raycast to check ground for jump

--- demo/blocks/source/comp/body.d
+++ demo/blocks/source/comp/body.d
@@ -14,25 +14,33 @@ class Character : Component, Updatable {
 
     private InputController controller;
     private DynamicBody bod;
+    private PhysicsManager phys;
 
     override void setup() {
         controller = entity.get_component!InputController();
         bod = entity.get_component!DynamicBody();
+        phys = entity.scene.get_manager!PhysicsManager().get;
 
         bod.max_speed = move_speed;
     }
 
     override void update() {
-        if (controller.jump.is_pressed) {
+        // check if on ground
+        auto rc = phys.raycast_from(bod, Vector3(0, -1, 0), Vector3(0, -1, 0), 1);
+        if (!rc.isNull) {
             // jump
-            bod.apply_impulse(Vector3(0, bod.mass * 16, 0), Vector3Zero);
+            if (controller.jump.is_pressed) {
+                bod.apply_impulse(Vector3(0, bod.mass * 16, 0), Vector3Zero);
+            }
         }
 
+        // movement
         if (raymath.Vector2LengthSqr(controller.move.value) > 0) {
             bod.apply_impulse(Vector3(controller.move.value.x * move_speed, 0,
                     controller.move.value.y * move_speed), Vector3Zero);
         }
 
+        // turning
         if (raymath.Vector2LengthSqr(controller.turn.value) > 0) {
             bod.apply_torque(Vector3(controller.turn.value.y * turn_speed * bod.mass,
                     0, -controller.turn.value.x * turn_speed * bod.mass));
