05c9c6972f88f97cbd9dc24099cc21d96b350a6f
redthing1 <redthing1@alt.icu>
2020-07-29 19:52:36 -0700

    add disabled lighting code

--- demo/blocks/source/play.d
+++ demo/blocks/source/play.d
@@ -11,9 +11,14 @@ import re.math;
 import comp.input;
 import comp.body;
 static import raylib;
+import rlights = re.gfx.lighting.rlights;
 
 /// simple 3d demo scene
 class PlayScene : Scene3D {
+    // private rlights.Light[rlights.MAX_LIGHTS] lights;
+    // private float light_angle = 6.282f;
+    // private Shader lighting_shader;
+
     override void on_start() {
         clear_color = Colors.LIGHTGRAY;
 
@@ -30,11 +35,32 @@ class PlayScene : Scene3D {
         // create a block, and assign it a physics object
         auto block = create_entity("block", Vector3(0, 5, 0));
         block.transform.orientation = Vector3(0, C_PI_4, C_PI_4); // euler angles
-        block.add_component(new Cube(Vector3(4, 4, 4), color_rgb(141, 113, 176)));
         block.add_component(new BoxCollider(Vector3(2, 2, 2), Vector3Zero));
         block.add_component(new DynamicBody(64));
         block.add_component!PlayerController();
         block.add_component!Character();
+
+        // // set up lighting
+        // lighting_shader = Core.content.load_shader("shader/basic_lighting.vert",
+        //         "shader/basic_lighting.frag");
+        // auto block_cube = block.add_component(new Cube(Vector3(4, 4, 4), color_rgb(141, 113, 176)));
+        // block_cube.model.materials[0].shader = lighting_shader;
+
+        // // Get some shader loactions
+        // lighting_shader.locs[raylib.ShaderLocationIndex.LOC_MATRIX_MODEL]
+        //     = raylib.GetShaderLocation(lighting_shader, "matModel");
+        // lighting_shader.locs[raylib.ShaderLocationIndex.LOC_VECTOR_VIEW]
+        //     = raylib.GetShaderLocation(lighting_shader, "viewPos");
+
+        // // ambient light level
+        // int ambient_loc = raylib.GetShaderLocation(lighting_shader, "ambient");
+        // auto ambient_val = [0.2f, 0.2f, 0.2f, 1.0f];
+        // raylib.SetShaderValue(lighting_shader, ambient_loc, &ambient_val,
+        //         raylib.ShaderUniformDataType.UNIFORM_VEC4);
+
+        // // create a point light
+        // lights[0] = rlights.CreateLight(rlights.LightType.LIGHT_POINT,
+        //         Vector3(4, 2, 4), Vector3Zero, Colors.WHITE, lighting_shader);
 
         // make small blocks
         enum small_block_count = 128;
@@ -54,7 +80,7 @@ class PlayScene : Scene3D {
 
             auto nt = create_entity("thing", Vector3(x_off, y_off, z_off));
             nt.transform.orientation = Vector3(x_ang, y_ang, z_ang); // euler angles
-            nt.add_component(new Cube(Vector3(1, 1, 1), color_rgb(209, 153, 56)));
+            auto lil_cube = nt.add_component(new Cube(Vector3(1, 1, 1), color_rgb(209, 153, 56)));
             nt.add_component(new BoxCollider(Vector3(0.5, 0.5, 0.5), Vector3Zero));
             auto thing_body = nt.add_component(new DynamicBody(2));
         }
@@ -62,5 +88,15 @@ class PlayScene : Scene3D {
         // point the camera at the block, then orbit it
         cam.look_at(block);
         cam.entity.add_component(new CameraFreeLook(block));
+    }
+
+    override void update() {
+        // import std.math : sin, cos;
+
+        // light_angle -= 0.02f;
+        // lights[0].position.x = cos(light_angle) * 4.0f;
+        // lights[0].position.z = sin(light_angle) * 4.0f;
+
+        // rlights.UpdateLightValues(lighting_shader, lights[0]);
     }
 }
