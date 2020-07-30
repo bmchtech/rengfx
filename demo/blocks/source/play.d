8bb2742a3d6a680480242b285c997652a029e419
xdrie <xdrie@alt.icu>
2020-07-29 19:56:08 -0700

    re enable lighting code

--- demo/blocks/source/play.d
+++ demo/blocks/source/play.d
@@ -15,9 +15,9 @@ import rlights = re.gfx.lighting.rlights;
 
 /// simple 3d demo scene
 class PlayScene : Scene3D {
-    // private rlights.Light[rlights.MAX_LIGHTS] lights;
-    // private float light_angle = 6.282f;
-    // private Shader lighting_shader;
+    private rlights.Light[rlights.MAX_LIGHTS] lights;
+    private float light_angle = 6.282f;
+    private Shader lighting_shader;
 
     override void on_start() {
         clear_color = Colors.LIGHTGRAY;
@@ -35,32 +35,33 @@ class PlayScene : Scene3D {
         // create a block, and assign it a physics object
         auto block = create_entity("block", Vector3(0, 5, 0));
         block.transform.orientation = Vector3(0, C_PI_4, C_PI_4); // euler angles
+        block.add_component(new Cube(Vector3(4, 4, 4), color_rgb(141, 113, 176)));
         block.add_component(new BoxCollider(Vector3(2, 2, 2), Vector3Zero));
         block.add_component(new DynamicBody(64));
         block.add_component!PlayerController();
         block.add_component!Character();
 
-        // // set up lighting
-        // lighting_shader = Core.content.load_shader("shader/basic_lighting.vert",
-        //         "shader/basic_lighting.frag");
-        // auto block_cube = block.add_component(new Cube(Vector3(4, 4, 4), color_rgb(141, 113, 176)));
-        // block_cube.model.materials[0].shader = lighting_shader;
+        // set up lighting
+        lighting_shader = Core.content.load_shader("shader/basic_lighting.vert",
+                "shader/basic_lighting.frag");
+        auto block_cube = block.add_component(new Cube(Vector3(4, 4, 4), color_rgb(141, 113, 176)));
+        block_cube.model.materials[0].shader = lighting_shader;
 
-        // // Get some shader loactions
-        // lighting_shader.locs[raylib.ShaderLocationIndex.LOC_MATRIX_MODEL]
-        //     = raylib.GetShaderLocation(lighting_shader, "matModel");
-        // lighting_shader.locs[raylib.ShaderLocationIndex.LOC_VECTOR_VIEW]
-        //     = raylib.GetShaderLocation(lighting_shader, "viewPos");
+        // Get some shader loactions
+        lighting_shader.locs[raylib.ShaderLocationIndex.LOC_MATRIX_MODEL]
+            = raylib.GetShaderLocation(lighting_shader, "matModel");
+        lighting_shader.locs[raylib.ShaderLocationIndex.LOC_VECTOR_VIEW]
+            = raylib.GetShaderLocation(lighting_shader, "viewPos");
 
-        // // ambient light level
-        // int ambient_loc = raylib.GetShaderLocation(lighting_shader, "ambient");
-        // auto ambient_val = [0.2f, 0.2f, 0.2f, 1.0f];
-        // raylib.SetShaderValue(lighting_shader, ambient_loc, &ambient_val,
-        //         raylib.ShaderUniformDataType.UNIFORM_VEC4);
+        // ambient light level
+        int ambient_loc = raylib.GetShaderLocation(lighting_shader, "ambient");
+        auto ambient_val = [0.2f, 0.2f, 0.2f, 1.0f];
+        raylib.SetShaderValue(lighting_shader, ambient_loc, &ambient_val,
+                raylib.ShaderUniformDataType.UNIFORM_VEC4);
 
-        // // create a point light
-        // lights[0] = rlights.CreateLight(rlights.LightType.LIGHT_POINT,
-        //         Vector3(4, 2, 4), Vector3Zero, Colors.WHITE, lighting_shader);
+        // create a point light
+        lights[0] = rlights.CreateLight(rlights.LightType.LIGHT_POINT,
+                Vector3(4, 2, 4), Vector3Zero, Colors.WHITE, lighting_shader);
 
         // make small blocks
         enum small_block_count = 128;
@@ -91,12 +92,14 @@ class PlayScene : Scene3D {
     }
 
     override void update() {
-        // import std.math : sin, cos;
+        super.update();
 
-        // light_angle -= 0.02f;
-        // lights[0].position.x = cos(light_angle) * 4.0f;
-        // lights[0].position.z = sin(light_angle) * 4.0f;
+        import std.math : sin, cos;
 
-        // rlights.UpdateLightValues(lighting_shader, lights[0]);
+        light_angle -= 0.02f;
+        lights[0].position.x = cos(light_angle) * 4.0f;
+        lights[0].position.z = sin(light_angle) * 4.0f;
+
+        rlights.UpdateLightValues(lighting_shader, lights[0]);
     }
 }
