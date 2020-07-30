7dedf99d628225c21c1a2755bf03e1f29ab5d828
xdrie <xdrie@alt.icu>
2020-07-29 19:28:43 -0700

    port rlights

---
+++ source/re/gfx/lighting/rlights.d
@@ -0,0 +1,153 @@
+module re.gfx.lighting.rlights;
+
+import raylib;
+
+/**********************************************************************************************
+*
+*   raylib.lights - Some useful functions to deal with lights data
+*
+*   CONFIGURATION:
+*
+*   enum RLIGHTS_IMPLEMENTATION
+*       Generates the implementation of the library into the included file.
+*       If not defined, the library is in header only mode and can be included in other headers 
+*       or source files without problems. But only ONE file should hold the implementation.
+*
+*   LICENSE: zlib/libpng
+*
+*   Copyright (c) 2017 Victor Fisac and Ramon Santamaria
+*
+*   This software is provided "as-is", without any express or implied warranty. In no event
+*   will the authors be held liable for any damages arising from the use of this software.
+*
+*   Permission is granted to anyone to use this software for any purpose, including commercial
+*   applications, and to alter it and redistribute it freely, subject to the following restrictions:
+*
+*     1. The origin of this software must not be misrepresented; you must not claim that you
+*     wrote the original software. If you use this software in a product, an acknowledgment
+*     in the product documentation would be appreciated but is not required.
+*
+*     2. Altered source versions must be plainly marked as such, and must not be misrepresented
+*     as being the original software.
+*
+*     3. This notice may not be removed or altered from any source distribution.
+*
+**********************************************************************************************/
+
+//----------------------------------------------------------------------------------
+// Defines and Macros
+//----------------------------------------------------------------------------------
+enum MAX_LIGHTS = 4; // Max lights supported by shader
+enum LIGHT_DISTANCE = 3.5f; // Light distance from world center
+enum LIGHT_HEIGHT = 1.0f; // Light height position
+
+//----------------------------------------------------------------------------------
+// Types and Structures Definition
+//----------------------------------------------------------------------------------
+enum LightType {
+    LIGHT_DIRECTIONAL,
+    LIGHT_POINT
+}
+
+struct Light {
+    bool enabled;
+    LightType type;
+    Vector3 position;
+    Vector3 target;
+    Color color;
+    int enabledLoc;
+    int typeLoc;
+    int posLoc;
+    int targetLoc;
+    int colorLoc;
+}
+
+/***********************************************************************************
+*
+*   RLIGHTS IMPLEMENTATION
+*
+************************************************************************************/
+
+//----------------------------------------------------------------------------------
+// Defines and Macros
+//----------------------------------------------------------------------------------
+// ...
+
+//----------------------------------------------------------------------------------
+// Types and Structures Definition
+//----------------------------------------------------------------------------------
+// ...
+
+//----------------------------------------------------------------------------------
+// Global Variables Definition
+//----------------------------------------------------------------------------------
+static Light[MAX_LIGHTS] lights;
+static int lightsCount = 0; // Current amount of created lights
+
+//----------------------------------------------------------------------------------
+// Module specific Functions Declaration
+//----------------------------------------------------------------------------------
+// ...
+
+//----------------------------------------------------------------------------------
+// Module Functions Definition
+//----------------------------------------------------------------------------------
+
+// Defines a light and get locations from PBR shader
+static void CreateLight(int type, Vector3 pos, Vector3 targ, Color color, Shader shader) {
+    Light light = {0};
+
+    if (lightsCount < MAX_LIGHTS) {
+        light.enabled = true;
+        light.type = cast(LightType) type;
+        light.position = pos;
+        light.target = targ;
+        light.color = color;
+
+        char[32] enabledName = "lights[x].enabled\0";
+        char[32] typeName = "lights[x].type\0";
+        char[32] posName = "lights[x].position\0";
+        char[32] targetName = "lights[x].target\0";
+        char[32] colorName = "lights[x].color\0";
+        enabledName[7] = cast(char)('0' + lightsCount);
+        typeName[7] = cast(char)('0' + lightsCount);
+        posName[7] = cast(char)('0' + lightsCount);
+        targetName[7] = cast(char)('0' + lightsCount);
+        colorName[7] = cast(char)('0' + lightsCount);
+
+        light.enabledLoc = GetShaderLocation(shader, cast(char*) enabledName);
+        light.typeLoc = GetShaderLocation(shader, cast(char*) typeName);
+        light.posLoc = GetShaderLocation(shader, cast(char*) posName);
+        light.targetLoc = GetShaderLocation(shader, cast(char*) targetName);
+        light.colorLoc = GetShaderLocation(shader, cast(char*) colorName);
+
+        UpdateLightValues(shader, light);
+
+        lights[lightsCount] = light;
+        lightsCount++;
+    }
+}
+
+// Send to PBR shader light values
+static void UpdateLightValues(Shader shader, Light light) {
+    // Send to shader light enabled state and type
+    SetShaderValue(shader, light.enabledLoc, &light.enabled, ShaderUniformDataType.UNIFORM_INT);
+    SetShaderValue(shader, light.typeLoc, &light.type, ShaderUniformDataType.UNIFORM_INT);
+
+    // Send to shader light position values
+    float[3] position = [light.position.x, light.position.y, light.position.z];
+    SetShaderValue(shader, light.posLoc, &position, ShaderUniformDataType.UNIFORM_VEC3);
+
+    // Send to shader light target position values
+    float[3] target = [light.target.x, light.target.y, light.target.z];
+    SetShaderValue(shader, light.targetLoc, &target, ShaderUniformDataType.UNIFORM_VEC3);
+
+    // Send to shader light color values
+    float[4] diff = [
+        cast(float) light.color.r / cast(float) 255,
+        cast(float) light.color.g / cast(float) 255,
+        cast(float) light.color.b / cast(float) 255,
+        cast(float) light.color.a / cast(float) 255
+    ];
+    SetShaderValue(shader, light.colorLoc, &diff, ShaderUniformDataType.UNIFORM_VEC4);
+}
