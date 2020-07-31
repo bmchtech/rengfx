62f186ef0b7eca38b29e3e587fe7fab543a9eba8
xdrie <xdrie@alt.icu>
2020-07-30 17:41:13 -0700

    blocks: add args for setting render resolution

--- demo/blocks/source/app.d
+++ demo/blocks/source/app.d
@@ -8,8 +8,12 @@ import std.stdio;
 import play;
 import hud;
 
+/// window width
 int width = 1280;
+/// window height
 int height = 720;
+/// factor by which to divide the render resolution
+int lowrez = 1;
 
 class Game : Core {
 	this() {
@@ -17,7 +21,7 @@ class Game : Core {
 	}
 
 	override void initialize() {
-		// default_resolution = Vector2(WIDTH / 2, HEIGHT / 2);
+		default_resolution = Vector2(width / lowrez, height / lowrez);
 		content.paths ~= ["../content/", "content/"];
 
 		load_scenes([new PlayScene(), new HUDScene()]);
@@ -27,7 +31,8 @@ class Game : Core {
 int main(string[] args) {
 	import std.getopt : getopt, defaultGetoptPrinter;
 
-	auto help = getopt(args, "width", &width, "height", &height);
+	auto help = getopt(args, "width", "window width", &width, "height", "window height",
+			&height, "lowrez", "factor by which to divide the render resolution", &lowrez);
 	if (help.helpWanted) {
 		defaultGetoptPrinter("Some information about the program.", help.options);
 		return 1;
