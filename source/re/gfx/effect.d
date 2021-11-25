8213b7bb161a5cf451b36587f85d37aeca949e66
redthing1 <redthing1@rie.icu>
2021-11-24 17:00:23 -0800

    fix effect template

--- source/re/gfx/effect.d
+++ source/re/gfx/effect.d
@@ -22,21 +22,21 @@ struct Effect {
         raylib.ShaderUniformDataType val_type;
         alias vartype = raylib.ShaderUniformDataType;
         static if (is(T == float)) {
-            val_type = vartype.UNIFORM_FLOAT;
+            val_type = vartype.SHADER_UNIFORM_FLOAT;
         } else static if (is(T == int)) {
-            val_type = vartype.UNIFORM_INT;
+            val_type = vartype.SHADER_UNIFORM_INT;
         } else static if (is(T == float[2])) {
-            val_type = vartype.UNIFORM_VEC2;
+            val_type = vartype.SHADER_UNIFORM_VEC2;
         } else static if (is(T == float[3])) {
-            val_type = vartype.UNIFORM_VEC3;
+            val_type = vartype.SHADER_UNIFORM_VEC3;
         } else static if (is(T == float[4])) {
-            val_type = vartype.UNIFORM_VEC4;
+            val_type = vartype.SHADER_UNIFORM_VEC4;
         } else static if (is(T == int[2])) {
-            val_type = vartype.UNIFORM_IVEC2;
+            val_type = vartype.SHADER_UNIFORM_IVEC2;
         } else static if (is(T == int[3])) {
-            val_type = vartype.UNIFORM_IVEC3;
+            val_type = vartype.SHADER_UNIFORM_IVEC3;
         } else static if (is(T == int[4])) {
-            val_type = vartype.UNIFORM_IVEC4;
+            val_type = vartype.SHADER_UNIFORM_IVEC4;
         } else {
             static assert(0, "unrecognized shader value data type");
         }
