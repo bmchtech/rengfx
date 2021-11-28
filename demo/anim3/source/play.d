ed7275b9c2fb7dfee640d064cce2f152c2ae47de
redthing1 <redthing1@rie.icu>
2021-11-27 21:01:22 -0800

    add stub host for model anim

--- demo/anim3/source/play.d
+++ demo/anim3/source/play.d
@@ -2,7 +2,7 @@ module play;
 
 import re;
 import re.gfx;
-import re.gfx.shapes.model;
+import re.gfx.shapes.anim_model;
 import re.gfx.shapes.grid;
 import re.gfx.shapes.cube;
 import re.ng.camera;
@@ -12,7 +12,7 @@ static import raylib;
 /// simple 3d demo scene
 class PlayScene : Scene3D {
     private PostProcessor glitch_postproc;
-    private float[2] aberrationOffset = [0.005, 0];
+    private float[2] aberrationOffset = [0.002, 0];
 
     override void on_start() {
         clear_color = Colors.LIGHTGRAY;
@@ -35,8 +35,10 @@ class PlayScene : Scene3D {
         cam.entity.position = Vector3(0, 3, 6);
 
         auto item1 = create_entity("item1", Vector3(0, 0, 0));
-        auto item1_asset = Core.content.load_model("models/lg/test1.glb");
-        auto item1_model = item1.add_component(new Model3D(item1_asset));
+        auto item1_asset_path = "models/lg/test3.glb";
+        auto item1_asset = Core.content.load_model(item1_asset_path);
+        auto item1_asset_anims = Core.content.load_model_animations(item1_asset_path);
+        auto item1_model = item1.add_component(new AnimModel3D(item1_asset, item1_asset_anims));
         item1.transform.scale = Vector3(2, 2, 2);
 
         cam.look_at(item1.transform.position + Vector3(0, 2, 0));
