00655f8c867370b1cb32d52f852fba29af26e3df
redthing1 <redthing1@rie.icu>
2021-11-27 20:31:04 -0800

    add anim3 demo

---
+++ demo/anim3/source/play.d
@@ -0,0 +1,63 @@
+module play;
+
+import re;
+import re.gfx;
+import re.gfx.shapes.model;
+import re.gfx.shapes.grid;
+import re.gfx.shapes.cube;
+import re.ng.camera;
+import re.math;
+static import raylib;
+
+/// simple 3d demo scene
+class PlayScene : Scene3D {
+    private PostProcessor glitch_postproc;
+    private float[2] aberrationOffset = [0.005, 0];
+
+    override void on_start() {
+        clear_color = Colors.LIGHTGRAY;
+
+        // load shader effects and add as a postprocessor
+        // auto ascii_shd = Effect(Core.content.load_shader(null, "shader/ascii.frag"), Colors.WHITE);
+        // ascii_shd.set_shader_var_imm("c_resolution", cast(float[2])[
+        //         resolution.x, resolution.y
+        //         ]);
+        // auto ascii_postproc = new PostProcessor(resolution, ascii_shd);
+        // postprocessors ~= ascii_postproc;
+
+        auto chrm_abr = Effect(Core.content.load_shader(null,
+                "shader/chromatic_aberration.frag"), color_alpha_white(0.8));
+        chrm_abr.set_shader_var("aberrationOffset", aberrationOffset);
+        glitch_postproc = new PostProcessor(resolution, chrm_abr);
+        postprocessors ~= glitch_postproc;
+
+        // set the camera position
+        cam.entity.position = Vector3(0, 3, 6);
+
+        auto item1 = create_entity("item1", Vector3(0, 0, 0));
+        auto item1_asset = Core.content.load_model("models/lg/test1.glb");
+        auto item1_model = item1.add_component(new Model3D(item1_asset));
+        item1.transform.scale = Vector3(2, 2, 2);
+
+        cam.look_at(item1.transform.position + Vector3(0, 2, 0));
+
+        // add a camera to look at the item1
+        // cam.entity.add_component(new CameraFreeLook(item1));
+
+        // draw a grid at the origin
+        auto grid = create_entity("grid");
+        grid.add_component(new Grid3D(20, 1));
+    }
+
+    override void update() {
+        super.update();
+
+        if (Input.is_mouse_pressed(MouseButton.MOUSE_BUTTON_LEFT)) {
+            if (Input.is_cursor_locked) {
+                Input.unlock_cursor();
+            } else {
+                Input.lock_cursor();
+            }
+        }
+    }
+}
