8979eca76824e7ab5568b044d690175c456786e9
redthing1 <redthing1@alt.icu>
2022-02-23 19:59:42 -0800

    stub cel2 demo

---
+++ demo/shanpes2/source/app.d
@@ -0,0 +1,35 @@
+module app;
+
+import std.stdio;
+
+import re;
+import re.math;
+import std.stdio;
+import play;
+import hud;
+
+static import raylib;
+
+class Game : Core {
+	enum WIDTH = 960;
+	enum HEIGHT = 540;
+
+	this() {
+		super(WIDTH, HEIGHT, "shanpes2");
+	}
+
+	override void initialize() {
+		default_resolution = Vector2(WIDTH, HEIGHT);
+		content.paths ~= ["../content/", "content/"];
+
+		raylib.SetConfigFlags(raylib.ConfigFlags.FLAG_MSAA_4X_HINT);
+
+		load_scenes([new PlayScene(), new HUDScene()]);
+	}
+}
+
+void main() {
+	auto game = new Game(); // init game
+	game.run();
+	game.destroy(); // clean up
+}
