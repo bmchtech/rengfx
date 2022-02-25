3fc037ac3bf80b433577caaa8cd2285fcac22036
redthing1 <redthing1@alt.icu>
2022-02-25 13:23:43 -0800

    disable debug render crosshair drawing

--- source/re/ng/diag/render.d
+++ source/re/ng/diag/render.d
@@ -12,19 +12,29 @@ static class DebugRender {
     public static Color debug_color_collider = Colors.GREEN;
 
     public static void default_debug_render(Renderable2D renderable) {
+        import re.math.rectangle_ext;
+        import std.algorithm.comparison: max;
+
+        // draw bounding rectangle
         raylib.DrawRectangleLinesEx(renderable.bounds, 1, debug_color);
+        // // draw center crosshair
+        // auto center = renderable.bounds.center;
+        // int crosshair_size = max(cast(int) (Core.default_resolution.x / 200), 1);
+        // raylib.DrawLineV(center + Vector2(-crosshair_size, 0), center + Vector2(crosshair_size, 0), debug_color);
+        // raylib.DrawLineV(center + Vector2(0, -crosshair_size), center + Vector2(0, crosshair_size), debug_color);
     }
 
+    /// draw 3d bounding box
     private static void draw_bounding_box(BoundingBox raw_box, ref Transform transform, Color color) {
         import std.math : abs;
 
         auto box = Bounds.calculate(raw_box, transform);
 
         auto size = Vector3(abs(box.max.x - box.min.x),
-                abs(box.max.y - box.min.y), abs(box.max.z - box.min.z));
+            abs(box.max.y - box.min.y), abs(box.max.z - box.min.z));
 
         auto center = Vector3(box.min.x + size.x / 2.0f, box.min.y + size.y / 2.0f,
-                box.min.z + size.z / 2.0f);
+            box.min.z + size.z / 2.0f);
 
         // TODO: we want something that supports transforms
 
@@ -42,17 +52,16 @@ static class DebugRender {
         // default_debug_render(renderable);
         auto comp = cast(Component) renderable;
         raylib.DrawModelWiresEx(model, comp.transform.position, comp.transform.axis_angle.axis,
-                comp.transform.axis_angle.angle * C_RAD2DEG, comp.transform.scale, debug_color_mesh);
+            comp.transform.axis_angle.angle * C_RAD2DEG, comp.transform.scale, debug_color_mesh);
 
         // check if renderable has colliders
         auto box_colls = comp.entity.get_components!BoxCollider;
         foreach (box; box_colls) {
             auto raw_bounds = BoundingBox( // min
-                    Vector3(-box.size.x + box.offset.x,
-                    -box.size.y + box.offset.y, -box.size.z + box.offset.z),
-                    // max
-                    Vector3(box.size.x + box.offset.x, box.size.y + box.offset.y,
-                        box.size.z + box.offset.z));
+                Vector3(-box.size.x + box.offset.x,
+                    -box.size.y + box.offset.y, -box.size.z + box.offset.z), // max
+                Vector3(box.size.x + box.offset.x, box.size.y + box.offset.y,
+                    box.size.z + box.offset.z));
             draw_bounding_box(raw_bounds, comp.entity.transform, debug_color_collider);
         }
     }
