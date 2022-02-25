9b0ff4e16014b58f4ae031594d5348be7b5aec9c
redthing1 <redthing1@alt.icu>
2022-02-25 00:58:34 -0800

    fix pong demo

--- demo/pong/source/play.d
+++ demo/pong/source/play.d
@@ -20,16 +20,16 @@ class PlayScene : Scene2D {
 
         auto paddle_sprite = new Sprite(paddle_tex);
 
-        auto ball_nt = create_entity("ball", Vector2(Core.window.width / 2, Core.window.height / 2));
+        auto ball_nt = create_entity("ball", Vector2(resolution.x / 2, resolution.y / 2));
         ball_nt.add_component(new SpriteRenderer(new Sprite(ball_tex)));
         auto ball = ball_nt.add_component!Ball();
 
-        auto player = create_entity("player", Vector2(Core.window.width / 2, Core.window.height - padding));
+        auto player = create_entity("player", Vector2(resolution.x / 2, resolution.y - padding));
         player.add_component(new SpriteRenderer(paddle_sprite));
         player.add_component!PlayerController();
         player.add_component!Paddle();
 
-        auto alice = create_entity("alice", Vector2(Core.window.width / 2, padding));
+        auto alice = create_entity("alice", Vector2(resolution.x / 2, padding));
         alice.add_component(new SpriteRenderer(paddle_sprite));
         alice.add_component!LogicController();
         alice.add_component!Paddle();
@@ -38,11 +38,11 @@ class PlayScene : Scene2D {
         ball.bounce_on(player.get_component!Paddle());
         ball.bounce_on(alice.get_component!Paddle());
 
-        auto pong = create_entity("pong", Vector2(padding, Core.window.height / 2));
+        auto pong = create_entity("pong", Vector2(padding, resolution.y / 2));
         auto pong_text = pong.add_component(new Text(Text.default_font, "pong", Text.default_size, Colors.WHITE));
         pong_text.set_align(Text.Align.Close, Text.Align.Center);
 
-        auto score = create_entity("score", Vector2(Core.window.width - padding, Core.window.height / 2));
+        auto score = create_entity("score", Vector2(resolution.x - padding, resolution.y / 2));
         auto score_text = score.add_component(new Text(Text.default_font, string.init, Text.default_size, Colors.WHITE));
         score_text.set_align(Text.Align.Far, Text.Align.Center);
         score.add_component!Scoreboard();
