f5cda85218a0cdf28526158d6c2e8480ee606cc7
redthing1 <redthing1@alt.icu>
2022-02-25 21:03:35 -0800

    new simplified quickshd with chromatic aberration demo

--- demo/content/shader/chromatic_aberration.frag
+++ demo/content/shader/chromatic_aberration.frag
@@ -4,26 +4,52 @@
 in vec2 fragTexCoord;
 in vec4 fragColor;
 
+// Input uniform values
 uniform sampler2D texture0;
 uniform vec4 colDiffuse;
 
 // Output fragment color
 out vec4 finalColor;
 
-// NOTE: Add here your custom variables
-uniform vec2 aberrationOffset;
+// user vars
+uniform vec3 i_resolution;
+uniform int i_frame;
+uniform float i_time;
+
+// shadertoy compat
+#define iResolution i_resolution
+#define iFrame i_frame
+#define iTime i_time
+
+#define T(u) texelFetch(tex, ivec2(pix_coord), 0);
+
+// shader vars
+uniform vec2 sample_offset;
+// vec2 sample_offset = vec2(0.005, 0.01);
+// #define bean 0
 
 vec4 sample(vec2 offset) {
-    return texture(texture0, fragTexCoord.xy - offset) * fragColor;
+  return texture(texture0, fragTexCoord.xy - offset) * fragColor;
+}
+
+vec4 frag_shader(sampler2D tex, vec2 frag_coord) {
+  // sample colors
+  vec2 uv = frag_coord / i_resolution.xy;
+
+  // vec4 texel_r = texture(tex, uv - sample_offset) * fragColor;
+  // vec4 texel_g = texture(tex, uv) * fragColor;
+  // vec4 texel_b = texture(tex, uv + sample_offset) * fragColor;
+
+  vec4 texel_r = sample(-sample_offset);
+  vec4 texel_g = sample(vec2(0, 0));
+  vec4 texel_b = sample(sample_offset);
+
+  return vec4(texel_r.r, texel_g.g, texel_b.b, texel_g.a);
 }
 
 void main() {
-    // sample colors
+  vec2 frag_coord = fragTexCoord.xy * i_resolution.xy;
+  vec4 draw_col = frag_shader(texture0, frag_coord);
 
-    vec4 texel_r = sample(-aberrationOffset);
-    vec4 texel_g = sample(vec2(0, 0));
-    vec4 texel_b = sample(aberrationOffset);
-
-    // final frag color
-    finalColor = vec4(texel_r.r, texel_g.g, texel_b.b, texel_g.a);
+  finalColor = draw_col;
 }
--- demo/quickshd/source/app.d
+++ demo/quickshd/source/app.d
@@ -3,10 +3,14 @@ module app;
 import std.stdio;
 
 import re;
+import re.gfx;
+import re.gfx.shapes.model;
+import re.gfx.shapes.grid;
+import re.gfx.shapes.cube;
+import re.ng.camera;
+import re.util.hotreload;
 import re.math;
-import std.stdio;
-import play;
-import hud;
+static import raylib;
 
 class Game : Core {
 	enum WIDTH = 960;
@@ -20,7 +24,48 @@ class Game : Core {
 		default_resolution = Vector2(WIDTH, HEIGHT);
 		content.paths ~= ["../content/", "content/"];
 
-		load_scenes([new PlayScene(), new HUDScene()]);
+		load_scenes([new PlayScene()]);
+	}
+}
+
+class PlayScene : Scene3D {
+	public Effect postfx1;
+
+	override void on_start() {
+		clear_color = Colors.LIGHTGRAY;
+
+		// set the camera position
+		cam.entity.position = Vector3(0, 10, 10);
+
+		auto block = create_entity("block", Vector3(0, 0, 0));
+		auto cube = block.add_component(new Cube(Vector3(2, 2, 2)));
+
+		// point the camera at the block, then orbit it
+		cam.look_at(block.position);
+		cam.entity.add_component(new CameraOrbit(block, 0.5));
+
+		// enable an example shader on cube
+		auto cross_stitch = new Effect(Core.content.load_shader(null,
+				"shader/cross_stitch.frag"), Colors.DARKGREEN);
+		auto mixAmt = 0.05f;
+		cross_stitch.set_shader_var("mixAmt", mixAmt);
+		cube.effect = cross_stitch;
+
+		// draw a grid at the origin
+		auto grid = create_entity("grid");
+		grid.add_component(new Grid3D(10, 1));
+
+		// add postprocessing
+		postfx1 = new Effect(new ReloadableShader(null, "shader/chromatic_aberration.frag"));
+		auto postfx1_pp = new PostProcessor(resolution, postfx1);
+		postprocessors ~= postfx1_pp;
+	}
+
+	override void update() {
+		super.update();
+
+		postfx1.update();
+		// postfx1.set_shader_var_imm("sample_offset", cast(float[2])[0.005, 0.05]);
 	}
 }
 
