766154dbb06f9c919f18eaf584da7ab6fe010b03
redthing1 <redthing1@alt.icu>
2022-11-12 16:02:26 -0800

    simplify demo

--- demo/nuidemo/source/app.d
+++ demo/nuidemo/source/app.d
@@ -19,40 +19,18 @@ class Game : Core {
 		auto_compensate_hidpi = true;
 		// auto_oversample_hidpi = true;
 
-		super(WIDTH, HEIGHT, "pksave explorer");
+		super(WIDTH, HEIGHT, "nuidemo");
 	}
 
 	override void initialize() {
-		// default_resolution = Vector2(WIDTH, HEIGHT);
-		content.paths ~= ["../content/", "content/", "./res/"];
-
+		content.paths ~= ["./content/", "./res/"];
 		raylib.SetWindowState(raylib.ConfigFlags.FLAG_WINDOW_RESIZABLE);
-
-		// auto_compensate_hidpi = true;
 		load_scenes([new GuiScene()]);
 	}
 }
 
 int main(string[] args) {
-	bool verbose;
-	auto help = getopt(args,
-		"verbose|v", &verbose,
-	);
-
-	if (help.helpWanted) {
-		defaultGetoptPrinter("Usage: ./a", help.options);
-		return 1;
-	}
-
 	raylib.SetWindowState(raylib.ConfigFlags.FLAG_WINDOW_HIGHDPI);
-
-	if (verbose) {
-		raylib.SetTraceLogLevel(raylib.TraceLogLevel.LOG_INFO);
-	} else {
-		raylib.SetTraceLogLevel(raylib.TraceLogLevel.LOG_WARNING);
-	}
-
-	// Game.auto_compensate_hidpi = false;
 
 	auto game = new Game(); // init game
 	game.run();
