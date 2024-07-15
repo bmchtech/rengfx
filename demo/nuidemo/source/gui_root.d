b29250ca6b656e7a64b4d9f2870c3b823f796a4f
redthing1 <redthing1@alt.icu>
2024-07-15 00:15:41 -0700

    update dray nuklear version

--- demo/nuidemo/source/gui_root.d
+++ demo/nuidemo/source/gui_root.d
@@ -9,7 +9,6 @@ import re.util.interop;
 
 import raylib;
 import raylib_nuklear;
-import nuklear_ext;
 import style;
 
 import std.array;
@@ -19,6 +18,21 @@ import std.string;
 // font size
 enum UI_FS = 16;
 enum UI_UI_PAD = 4;
+
+// utility to draw tabs for nuklear
+int nk_tab(nk_context* ctx, const char* title, int active) {
+    auto f = cast(nk_user_font*) ctx.style.font;
+    float text_width = f.width(f.userdata, f.height, title, nk_strlen(title));
+    float widget_width = text_width + 3 * ctx.style.button.padding.x;
+    nk_layout_row_push(ctx, widget_width);
+    auto c = ctx.style.button.normal;
+    if (active) {
+        ctx.style.button.normal = ctx.style.button.active;
+    }
+    int r = nk_button_label(ctx, title);
+    ctx.style.button.normal = c;
+    return r;
+}
 
 class GuiRoot : Component, Renderable2D, Updatable {
     mixin Reflect;
