dcffc37d833fd36be3562d9485aa574d68fb175f
redthing1 <redthing1@alt.icu>
2024-07-15 20:26:31 -0700

    pong: tweak resolution

--- demo/pong/source/app.d
+++ demo/pong/source/app.d
@@ -1,15 +1,20 @@
 import std.stdio;
 
 import re;
+import re.math;
 import std.stdio;
 import play;
 
 class Game : Core {
+	enum WIDTH = 480;
+	enum HEIGHT = 800;
+
 	this() {
-		super(360, 640, "pong");
+		super(WIDTH, HEIGHT, "pong");
 	}
 
 	override void initialize() {
+		default_resolution = Vector2(WIDTH / 2, HEIGHT / 2);
 		content.paths ~= ["../content/", "content/"];
 
 		load_scenes([new PlayScene()]);
