e27cef4cdf70caf81b283b59a231e9c75935906b
redthing1 <redthing1@alt.icu>
2024-07-16 12:50:29 -0700

    nuidemo: use point filtering on font texture

--- demo/nuidemo/source/gui_root.d
+++ demo/nuidemo/source/gui_root.d
@@ -49,6 +49,7 @@ class GuiRoot : Component, Renderable2D, Updatable {
     override void setup() {
         bg = ColorToNuklearF(Colors.SKYBLUE);
         auto ui_font = raylib.LoadFontEx("./res/SourceSansPro-Regular.ttf", UI_FS, null, 0);
+        raylib.SetTextureFilter(ui_font.texture, raylib.TextureFilter.TEXTURE_FILTER_POINT);
         ctx = InitNuklearEx(ui_font, UI_FS);
         ui_scale = Core.window.dpi_scale * Core.render_oversample_factor;
         SetNuklearScaling(ctx, ui_scale);
